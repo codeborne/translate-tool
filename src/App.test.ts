73fc9de05aab1562bbb9d3829aec16d5c279a1ee
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-10-29 10:35:51 +0300

    remove template content

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,11 +1,11 @@
-import {render} from '@testing-library/svelte';
-import {expect} from 'chai';
-import App from './App.svelte';
+import {render} from '@testing-library/svelte'
+import {expect} from 'chai'
+import App from './App.svelte'
 
 describe('<App>', () => {
-  it('renders learn svelte link', () => {
-    const {getByText} = render(App);
-    const linkElement = getByText(/learn svelte/i);
-    expect(document.body.contains(linkElement));
-  });
-});
+  it('renders', () => {
+    const {getByText} = render(App)
+    const e = getByText(/Translate/i)
+    expect(document.body.contains(e))
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import App from './App.svelte';
+import App from './App.svelte'
 
-var app = new App({
-  target: document.body,
-});
+const app = new App({target: document.body,})
 
-export default app;
+export default app
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
+  import.meta.hot.accept()
   import.meta.hot.dispose(() => {
-    app.$destroy();
-  });
+    app.$destroy()
+  })
 }

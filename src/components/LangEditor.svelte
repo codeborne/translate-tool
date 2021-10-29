426fad168ed254f8ae246875d71a0e13c3dabab7
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-10-29 14:25:01 +0300

    add input prototype

--- src/components/LangEditor.svelte
+++ src/components/LangEditor.svelte
@@ -2,7 +2,10 @@
   export let lang: string
 
   let dict: Record<string, any>
-  $: fetch(`/i18n/${lang}.json`).then(r => r.json()).then(r => dict = r)
+  $: {
+    fetch(`/i18n/${lang}.json`).then(r => r.json()).then(r => dict = r)
+    // console.log(dict)
+  }
 </script>
 
 <div>
@@ -12,5 +15,22 @@
   {lang}
 
   {JSON.stringify(dict)}
+  <br>
+
+  {#if dict != undefined}
+    {#each Object.keys(dict) as value, i}
+      {#if typeof dict[value] === 'object' && dict[value] != null}
+      <label>{value}
+        <input value="this is an object"/>
+      </label>
+      {:else}
+        <label>{value}
+          <input value={dict[value]}/>
+        </label>
+        {/if}
+
+      <br>
+    {/each}
+  {/if}
 
 </div>

dcc30100180cb6ad1b7a366be8048e34dfec2158
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-10-29 16:28:12 +0300

    add conditional statement for comparing values

--- src/components/ObjectInput.svelte
+++ src/components/ObjectInput.svelte
@@ -7,11 +7,15 @@
 </script>
 
 
-  {#each Object.keys(dict) as value, i}
+  {#each Object.keys(dict) as value}
     {#if typeof dict[value] === 'object' && dict[value] != null}
         <svelte:self dict={dict[value]} configSet={configSet[value]}/>
     {:else}
-      <Input title={value} value={dict[value]} original={configSet[value]}/>
+      {#if dict[value] != configSet[value]}
+        <Input title={value} value={dict[value]} original={configSet[value]}/> // wip
+      {:else}
+        <Input title={value} value={dict[value]} original={configSet[value]}/>
+      {/if}
     {/if}
   {/each}
 

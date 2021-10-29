f24fae6dfe3a19af72853e76d8cf29f3367bed69
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-10-29 10:42:24 +0300

    display available languages

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+  import langs from '../i18n/langs.json'
+  import en from '../i18n/en.json'
+  import et from '../i18n/et.json'
 </script>
 
 <div class="container text-center">
   Hello from Translate tool
+  <br>
+  Available langs: {langs.join(', ')}
 </div>

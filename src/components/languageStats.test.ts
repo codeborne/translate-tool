5e1682b25b3adc4fd5995a5aa13bf4dac4ae26f2
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-01 17:22:41 +0200

    add tests for languageStats

---
+++ src/components/languageStats.test.ts
@@ -0,0 +1,32 @@
+import {getTotalDictCount, getFilledDictCount} from './languageStats'
+import {expect} from 'chai'
+
+describe('getTotalDictAmount', () => {
+  it('returns correct total count', () => {
+    const dict = getTotalDictCount({
+      "1": 1, "2": 2,
+      "obj1": {
+        "3": 3, "4": 4,
+        "obj2": {
+          "5": 5, "6": 6
+        }
+      }
+    })
+    expect(dict).to.deep.equal(6)
+  })
+})
+
+describe('getFilledDictCount', () => {
+  it('returns correct total count with filled fields', () => {
+    const dict = getFilledDictCount({
+      "1": 1, "2": 2,
+      "obj1": {
+        "3": 3, "4": '',
+        "obj2": {
+          "5": '', "6": ''
+        }
+      }
+    })
+    expect(dict).to.deep.equal(3)
+  })
+})

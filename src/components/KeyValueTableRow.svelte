e5175d5f9f77d6f46c23ae26706363c9c06f2717
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-01 11:53:31 +0200

    use better names, simplify code and display full keys

--- src/components/KeyValueTableRow.svelte
+++ src/components/KeyValueTableRow.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   export let dict: Record<string, any>
-  export let configSet: Record<any, string>
+  export let defaultDict: Record<any, string>
+  export let keyPrefix = ''
+
+  const fullKey = (key: string) => (keyPrefix ? keyPrefix + '.' : '') + key
 </script>
 
-{#each Object.keys(configSet) as key}
-  {#if typeof configSet[key] === 'object' && configSet[key] != null}
-    <svelte:self dict={dict[key] ??= {}} configSet={configSet[key]}/>
+{#each Object.keys(defaultDict) as key}
+  {#if typeof defaultDict[key] === 'object' && defaultDict[key]}
+    <svelte:self keyPrefix={fullKey(key)} dict={dict[key] ??= {}} defaultDict={defaultDict[key]}/>
   {:else}
     <tr>
-      <td>{key}</td>
+      <td>{fullKey(key)}</td>
       <td><input bind:value={dict[key]} placeholder={dict[key]}></td>
-      <td>{configSet[key]}</td>
+      <td>{defaultDict[key]}</td>
     </tr>
   {/if}
 {/each}
--- src/components/LangEditor.svelte
+++ src/components/LangEditor.svelte
@@ -3,56 +3,51 @@
   import {onMount} from 'svelte';
 
   // todo make configurable
-  let config: string = 'en'
-  let configSet: Record<string, any>
-  let changedSet: Record<string, any>
-
-  onMount(() => {
-    fetch(`/i18n/${config}.json`)
-      .then(res => res.json())
-      .then(data => {
-        configSet = data
-      })
-  })
-
-  function save() {
-    console.log('saved')
-  }
-
+  let defaultLang: string = 'en'
   export let lang: string
 
+  let defaultDict: Record<string, any>
   let dict: Record<string, any>
-  $: if (lang) load(lang)
+
+  onMount(async () => {
+    defaultDict = await load(defaultLang)
+  })
+
+  $: if (lang) (async function() {
+    dict = await load(lang)
+  })()
+
+  function save() {
+    console.log('saved', dict)
+  }
 
   function load(lang: string) {
-    fetch(`/i18n/${lang}.json`).then(r => r.json()).then(r => dict = r)
+    return fetch(`/i18n/${lang}.json`).then(r => r.json())
   }
 </script>
 
 <div class="mt-3">
   <h5>Currently editing: {lang}</h5>
 
-  <br>
-  {#if dict != undefined && configSet != undefined}
+  {#if dict && defaultDict}
     <table class="table">
       <thead>
       <tr>
         <th scope="col">Name</th>
         <th scope="col">Selected ( {lang} )</th>
-        <th scope="col">Default ( {config} )</th>
+        <th scope="col">Default ( {defaultLang} )</th>
       </tr>
       </thead>
       <tbody>
-      <KeyValueTableRow dict={dict} configSet={configSet}/>
+        <KeyValueTableRow {dict} {defaultDict}/>
       </tbody>
     </table>
 
     <button on:click={save} class="btn btn-primary mt-3 mb-5 ">Save Changes</button>
 
-    <br>
     <h3>RAW output:</h3>
     <pre>
-      {JSON.stringify(  dict, null, 4)}
+      {JSON.stringify(dict, null, 4)}
     </pre>
   {/if}
 

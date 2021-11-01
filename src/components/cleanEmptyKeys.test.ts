d12f717e9cce2a5a6f119243b5ef145c9d2674fe
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-01 16:02:02 +0200

    add additional tests for cleanEmptyKeys

--- src/components/cleanEmptyKeys.test.ts
+++ src/components/cleanEmptyKeys.test.ts
@@ -6,4 +6,9 @@ describe('cleanEmptyKeys', () => {
     const dict = cleanEmptyKeys({hello: {empty: '', notEmpty: '123'}})
     expect(dict).to.deep.equal({hello: {notEmpty: '123'}})
   })
+
+  it('removes empty objects', () => {
+    expect(cleanEmptyKeys({hello: {empty: {}, notEmpty: 1}})).to.deep.equal({hello: {notEmpty: 1}})
+    expect(cleanEmptyKeys({hello: {empty: {}, notEmpty: {empty: ''}}})).to.deep.equal({})
+  })
 })

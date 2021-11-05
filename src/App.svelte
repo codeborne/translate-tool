d41d9c24f462d2fd5c7de98040831cf6318c9532
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-05 15:37:25 +0200

    add initial language importer

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import LangSwitcher from './components/LangSwitcher.svelte'
   import LangEditor from './components/LangEditor.svelte'
+  import LangImporter from './LangImporter.svelte'
   import langs from '../i18n/langs.json'
+
+  let displayLangImporter: boolean = true;
 
   let lang: string
 </script>
@@ -9,8 +12,15 @@
 <div class="container">
   <h1>Translation Tool</h1>
   <br>
+  {#if !displayLangImporter}
+    <button type="button" class="btn btn-primary" on:click={() => (displayLangImporter = true)}>
+      Import config
+    </button>
 
-  <LangSwitcher {langs} bind:lang/>
+    <LangSwitcher {langs} bind:lang/>
 
-  <LangEditor {lang}/>
+    <LangEditor {lang}/>
+  {:else}
+    <LangImporter bind:isOpen={displayLangImporter}/>
+  {/if}
 </div>

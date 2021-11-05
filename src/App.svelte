5cdc6eadf16df108f0f0e1a7a9d3a9cf8ca01ab7
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-05 16:57:38 +0200

    add validation to importer

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
   import Navbar from "./components/Navbar.svelte";
 
   let displayLangImporter: boolean = true;
-  let url: string
+  let url = ''
 
   let lang: string
 </script>
--- src/components/LangImporter.svelte
+++ src/components/LangImporter.svelte
@@ -1,6 +1,35 @@
-<script>
-  export let isOpen
-  export let url
+<script lang="ts">
+  export let isOpen: boolean
+  export let url: string
+  let warning = ''
+
+  async function submit() {
+    warning = ''
+    if (url) {
+      let dict = await fetchUrl(url)
+      if (validate(dict)) {
+        isOpen = false
+        warning = ''
+      }
+    } else {
+      warning = 'Input must not be empty'
+    }
+  }
+
+  const fetchUrl = (link) => fetch(link).then(r => r.json()).catch((e) => warning = e)
+
+  function validate(arr: any) {
+    if (arr) {
+      if (!Array.isArray(arr)) {
+        warning = 'Language list file must be an array'
+        return false
+      }
+      return true
+    } else {
+      warning = 'Invalid file'
+      return false
+    }
+  }
 </script>
 
 
@@ -10,5 +39,8 @@
     <input type="text" placeholder="url link here" bind:value={url} class="form-control" id="text" aria-describedby="url">
     <div id="url" class="form-text">You can change it at any time.</div>
   </div>
-  <button on:click={() => isOpen = false} type="button" class="btn btn-primary">Import</button>
+  <button on:click={submit} type="button" class="btn btn-primary">Import</button>
+  {warning}
+
+  <div>Example link: '../../i18n/langs.json'</div>
 </form>

460a3b37d878b84f30e9c2af3bee98dafbf719fc
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-05 15:56:20 +0200

    remove comment

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,9 +10,6 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-<!--        <li class="nav-item">-->
-<!--          <a class="nav-link active" aria-current="page" href="#">Home</a>-->
-<!--        </li>-->
       </ul>
       {#if !showConfigButton}
         <button type="button" class="btn btn-primary" on:click={() => (showConfigButton = true)}>

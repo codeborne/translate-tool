454334bd4aa9a128f80bcc2a8d66e52be7f68954
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-05 11:45:42 +0200

    add additional test for languageStats.test.ts

--- src/components/languageStats.test.ts
+++ src/components/languageStats.test.ts
@@ -4,9 +4,10 @@ import {expect} from 'chai'
 describe('getTotalDictAmount', () => {
   it('returns correct total count', () => {
     const dict = getTotalDictCount({
-      "1": 1, "2": 2, "obj1": {"3": 3, "4": 4, "obj2": {"5": 5, "6": 6}}})
+      t1: 1, t2: 2, t: {t3: 3, t4: 4, t: {t5: 5, t6: 6}}})
     expect(dict).to.deep.equal(6)
     expect(getTotalDictCount({})).to.deep.equal(0)
+    expect(getTotalDictCount({hello: 'world', nested: {hello: 'Another World'}})).to.deep.equal(2)
   })
 })
 

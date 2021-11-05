0c7228d2a13a6190d2a2fb3dca95e2e64aad38de
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-05 12:12:24 +0200

    add tests for LangSwticher.test.ts

---
+++ src/components/LangSwticher.test.ts
@@ -0,0 +1,21 @@
+import {act, render} from '@testing-library/svelte'
+import {expect} from 'chai'
+import LangEditor from './LangEditor.svelte'
+import {stub} from 'sinon'
+import LangSwitcher from './LangSwitcher.svelte'
+
+describe('<LangSwitcher>', () => {
+  const langs = ['en', 'et', 'es', 'de', 'ru', 'se']
+
+  it('renders list', () => {
+    const {container} = render(LangSwitcher, {lang: 'de', langs})
+    expect(container.querySelector('select'))
+  })
+
+  it('renders list as defined by available languages', () => {
+    const {container} = render(LangSwitcher, {lang: 'de', langs})
+    expect(container.querySelectorAll('select option')).to.have.length(6)
+    expect(container.querySelector('select')!.value).contains('de')
+    expect(container.querySelector('select')!.textContent).contains('EN')
+  })
+})

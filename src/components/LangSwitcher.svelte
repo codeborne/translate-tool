e3ab1aed546178e4902526d5df6698990c005579
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-08 15:00:01 +0200

    modal is now centered

--- src/components/LangSwitcher.svelte
+++ src/components/LangSwitcher.svelte
@@ -26,7 +26,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="confirmModalLabel">Uncopied changes</h5>

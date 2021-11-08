64f8a26775738cf207711b38f50fe7cb277b3a0f
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-08 11:04:19 +0200

    language now changes on button click

--- src/components/LangSwitcher.svelte
+++ src/components/LangSwitcher.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   export let langs: string[]
   export let lang = langs[0]
+  let selected:string = lang
+
+  function selectLang() {
+    lang = selected
+  }
 </script>
 
 <div class="container">
-  <select bind:value={lang} class="form-select">
+  <select bind:value={selected} class="form-select">
     {#each langs as lang}
       <option value={lang}>{lang.toUpperCase()}</option>
     {/each}
   </select>
-  <button class="btn btn-primary mt-3">Change Language</button>
+  <button on:click={selectLang} class="btn btn-primary mt-3">Change Language</button>
 </div>

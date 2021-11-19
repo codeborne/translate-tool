025a69915b47261d1a4b7b677f80a4a73af1a205
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-19 13:33:56 +0200

    add github fetching via token

--- src/components/LangImporter.svelte
+++ src/components/LangImporter.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   export let isOpen: boolean
   export let url: string
+  export let githubUrl: string = 'https://api.github.com/repos/Uptaker/access-token-test/contents/i18n/langs.json'
+  export let token: string = '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'
   export let langs: Record<string, any>
   let warning = ''
 
-  async function submit() {
+  async function submitPublic() {
     warning = ''
     if (url) {
-      let dict = await fetchUrl(url)
+      let dict = fetchUrl(url)
       if (validate(dict)) {
         langs = dict
         saveToLocalStorage()
@@ -19,9 +21,29 @@
     }
   }
 
+  async function submitGithub() {
+    warning = ''
+    if (githubUrl) {
+      if (token) {
+        console.log('token exists')
+        let dict = await fetchGithubUrl(githubUrl, token)
+        dict = JSON.parse(atob(dict.content))
+        if (validate(dict)) {
+          langs = dict
+          saveToLocalStorage()
+          isOpen = false
+          warning = ''
+        }
+      } else {
+          warning = 'Token must not be empty'
+        }
+    } else {
+      warning = 'Link must not be empty'
+    }
+  }
+
   function saveToLocalStorage() {
     localStorage.clear()
-
     let data = {
       url,
       langs,
@@ -30,6 +52,22 @@
   }
 
   const fetchUrl = (link) => fetch(link).then(r => r.json()).catch((e) => warning = e)
+
+  function fetchGithubUrl(link, token) {
+    const headers = new Headers({
+      'Authorization': `token ${token}`,
+      'Accept': 'application/vnd.github.VERSION.raw'
+    });
+    return fetch(githubUrl, {
+      method: 'GET',
+      headers,
+    }).then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(response.text() as string)
+      }}).catch((err) => warning = err)
+  }
 
   function validate(arr: any) {
     if (arr) {
@@ -49,11 +87,22 @@
 <form class="container">
   <div class="mb-3" >
     <label for="url" class="form-label">Import a custom configuration</label>
-    <input type="text" placeholder="url link here" bind:value={url} class="form-control" id="text" aria-describedby="url">
+    <input type="text" placeholder="url link here" bind:value={url} class="form-control" aria-describedby="url">
     <div id="url" class="form-text">You can change it at any time.</div>
-  </div>
-  <button on:click={submit} type="button" class="btn btn-primary">Import</button>
-  {warning}
+    <div>Example link: '../../i18n/langs.json'</div>
+    <button on:click={submitPublic} type="button" class="btn btn-primary">Import</button>
 
-  <div>Example link: '../../i18n/langs.json'</div>
+    <br/>
+    <br/>
+    <label for="url" class="form-label">Import via GitHub</label>
+    <input type="text" placeholder="github raw url" bind:value={githubUrl} class="form-control" aria-describedby="url">
+    <div>Example link: 'https://raw.githubusercontent.com/Uptaker/access-token-test/main/i18n/langs.json'</div>
+
+    <br/>
+
+    <label for="url" class="form-label">Authorization token</label>
+    <input type="text" placeholder="github raw url" bind:value={token} class="form-control" aria-describedby="url">
+  </div>
+  <button on:click={submitGithub} type="button" class="btn btn-primary">Import</button>
+  {warning}
 </form>

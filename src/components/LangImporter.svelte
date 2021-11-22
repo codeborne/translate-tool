d728be919940d621518dea66b2ae62d3e7bf01ac
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-22 16:56:46 +0200

    Split LangImporter into multiple components for easier project management

--- src/components/LangImporter.svelte
+++ src/components/LangImporter.svelte
@@ -1,149 +1,18 @@
 <script lang="ts">
-  import {b64DecodeUnicode} from '../utils'
+  import ProjectSettings from "./ProjectSettings.svelte";
+  import AddProjectPublic from "./AddProjectPublic.svelte";
+  import AddProjectFromRepository from "./AddProjectFromRepository.svelte";
   export let isOpen: boolean
-  let url: string = ''
-  export let token: string = ''
-  export let langs: Record<string, any>
-  let warning = ''
-  let username: string = 'paywerk'
-  let repo: string = 'paywerk'
-  let structure: string = '/i18n/common'
-
-  async function submitPublic() {
-    warning = ''
-    if (url) {
-      let dict = await fetchUrl(url)
-      if (validate(dict)) {
-        langs = dict
-        await saveToLocalStorage(url, true)
-        isOpen = false
-        warning = ''
-      }
-    } else {
-      warning = 'Input must not be empty'
-    }
-  }
-
-  async function submitGithub() {
-    warning = ''
-    if (isGithubFormValid()) {
-      let dictUrl = `https://api.github.com/repos/${username}/${repo}/contents${structure}/langs.json`
-      let dict = await fetchGithubUrl(dictUrl, token)
-      console.log(b64DecodeUnicode(dict.content))
-      if (dict) {
-        dict = JSON.parse(b64DecodeUnicode(dict.content)) // content is base64 encoded and required decoding
-        if (validate(dict)) {
-          langs = dict
-          saveToLocalStorage(dictUrl, false)
-          isOpen = false
-          warning = ''
-        }
-      }
-    }
-  }
-
-  function isGithubFormValid() {
-    if (username) {
-      if (token) {
-        if (structure) {
-          if (repo) {
-            return true
-          } else {
-            warning = 'Repository name must not be empty'
-          }
-        } else {
-          warning = 'Project location must not be empty'
-        }
-      } else {
-        warning = 'Token must not be empty'
-      }
-    } else {
-      warning = 'Link must not be empty'
-    }
-    return false
-  }
+  export let storage: any[]
+  export let selected: string
 
 
-  function saveToLocalStorage(link: string, isPublic: boolean) {
-    localStorage.clear()
-    let data = {
-      url: link,
-      langs,
-      isPublic,
-      token
-    }
-    localStorage.setItem('data', JSON.stringify(data))
-  }
-
-  const fetchUrl = (link) => fetch(link).then(r => r.json()).catch((e) => warning = e)
-
-  function fetchGithubUrl(link, token) {
-    const headers = new Headers({'Authorization': `token ${token}`});
-    return fetch(link, { method: 'GET', headers})
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          return response.text().then(text => {
-            throw (JSON.parse(text))
-          })}})
-      .catch((err) => {
-        warning = err.message
-      })
-  }
-
-  function validate(arr: any) {
-    if (arr) {
-      if (!Array.isArray(arr)) {
-        warning = 'Language list file must be an array'
-        return false
-      }
-      return true
-    } else {
-      warning = 'Invalid file'
-      return false
-    }
-  }
 </script>
-
-{#if warning}
-<div class="warning p-3 mb-3">
-  {warning}
-</div>
+{#if storage.length > 0}
+  <ProjectSettings bind:storage />
 {/if}
-
-<div class="outline p-3 mb-3 d-flex flex-column justify-content-center align-items-center">
-  <h5 class="mb-4">Import a public dictionary</h5>
-  <div class="mb-3" >
-    <label for="url" class="form-label">Public configuration link</label>
-    <input type="text" placeholder="url link" bind:value={url} class="form-control" aria-describedby="url">
-    <div id="url" class="form-text mb-4">You can change it at any time. Example link: <i>../../i18n/langs.json</i></div>
-    </div>
-  <button on:click={submitPublic} type="button" class="btn btn-primary w-auto">Import</button>
-</div>
-
-<div class="outline p-3 mb-3 d-flex flex-column justify-content-center align-items-center">
-  <h5 class="mb-4">Import a private dictionary via GitHub</h5>
-  <div class="mb-3" >
-    <label for="url" class="form-label">Repository owner's username</label>
-    <input type="text" placeholder="repo owner" bind:value={username} class="form-control" aria-describedby="url">
-    <div class="form-text mb-4">eg. <b>codeborne</b> for <i>https://github.com/<b>codeborne</b></i></div>
-
-    <label for="url" class="form-label">Repository name</label>
-    <input type="text" placeholder="repo name" bind:value={repo} class="form-control" aria-describedby="url">
-    <div class="form-text mb-4">eg. <b>translate-tool</b> for <i>https://github.com/codeborne/<b>translate-tool</b></i></div>
-
-    <label for="url" class="form-label">Project location</label>
-    <input type="text" placeholder="project structure" bind:value={structure} class="form-control" aria-describedby="url">
-    <div class="form-text mb-4">Where the project is located within the root repository. eg <b>/i18n</b> or <b>/i18n/merchant</b></div>
-
-
-    <label for="url" class="form-label">Authorization token</label>
-    <input type="text" placeholder="auth token" bind:value={token} class="form-control" aria-describedby="url">
-    <div class="form-text mb-4">This token will be used to access the private repository</div>
-  </div>
-  <button on:click={submitGithub} type="button" class="btn btn-primary w-auto">Import</button>
-</div>
+<AddProjectPublic bind:storage bind:isOpen bind:selected />
+<AddProjectFromRepository bind:storage bind:isOpen bind:selected />
 
 <style>
   .outline {

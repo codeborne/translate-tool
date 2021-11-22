566bfb20b5ec2de71aba6369bbfd3ea20ccadddc
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-22 16:53:12 +0200

    Add project settings component for managing components

---
+++ src/components/ProjectSettings.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  export let storage: any[]
+  let showEditor: boolean = false
+  let select: string
+  let title: string
+  let indent: number
+  let defaultLang: string
+
+  function deleteFromStorage() {
+    storage = storage.filter(obj => obj.title !== select)
+    localStorage.setItem('config', JSON.stringify(storage))
+  }
+
+  function toggleForm() {
+    showEditor = !showEditor
+  }
+
+  function saveChangesToProject() {
+    let project: Record<string, any> = storage.find(obj => obj.title === select)
+    let filteredStorage: any[] = storage.filter(obj => obj.title !== select)
+    project.title = title
+    project.indent = indent as number
+    project.defaultLang = defaultLang
+    filteredStorage.push(project)
+    localStorage.setItem('config', JSON.stringify(filteredStorage))
+    storage = filteredStorage
+    toggleForm()
+  }
+
+  function setFormInputs() {
+    let project = storage.find(obj => obj.title === select)
+    title = project.title
+    indent = project.indent
+    defaultLang = project.defaultLang
+  }
+
+  $: if (select) setFormInputs()
+
+</script>
+
+<div class="outline p-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+  <h5 class="mb-4">Manage Projects</h5>
+  <div class="mb-3" >
+      <select bind:value={select} class="form-select" aria-label="Select project">
+        {#each storage as obj }
+          <option value="{obj.title}">{obj.title}</option>
+        {/each}
+      </select>
+  </div>
+  {#if showEditor}
+    <div class="d-flex flex-column justify-content-center align-items-center">
+    <label class="form-label">Project name</label>
+    <input type="text" placeholder="project name" bind:value={title} class="form-control mb-4" aria-describedby="url">
+
+    <label class="form-label">Indent spaces</label>
+    <input type="number" placeholder="space indent" bind:value={indent} class="form-control mb-4" aria-describedby="url">
+
+    <label class="form-label">Default language</label>
+    <input type="text" placeholder="space indent" bind:value={defaultLang} class="form-control" aria-describedby="url">
+      <div class="form-text mb-4"><i>May depend on your configuration, eg 'en' for English.</i></div>
+
+    </div>
+    <div class="d-flex gap-5 mt-3">
+    <button on:click={toggleForm} type="button" class="btn btn-primary w-auto">Cancel</button>
+    <button on:click={saveChangesToProject} type="button" class="btn btn-primary w-auto">Save</button>
+    </div>
+  {:else}
+    <div class="d-flex gap-5 mt-3">
+    <button on:click={toggleForm} type="button" class="btn btn-primary w-auto">Edit</button>
+      <button on:click={deleteFromStorage} type="button" class="btn btn-danger w-auto">Delete</button>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .outline {
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    background-color: white;
+  }
+
+  .warning {
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    background-color: #F9D8D8;
+    text-align: center;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: #404142;
+  }
+</style>

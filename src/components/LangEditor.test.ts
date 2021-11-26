ee6fb744976e2cfeee8c7d680188d60b09c2552a
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-26 16:47:14 +0200

    #180444539 Readjust LangEditor.test.ts

--- src/components/LangEditor.test.ts
+++ src/components/LangEditor.test.ts
@@ -5,22 +5,22 @@ import {stub} from 'sinon'
 
 describe('<LangEditor>', () => {
   const dict = {hello: 'world', nested: {hello: 'Another World'}}
+  const selectedProject = 'testproject'
+  const project = {isPublic: true, title: 'project', langs: ['en', 'et'], indent: 2, defaultLang: 'en', url: '../../i18n/langs.json'}
 
   it('renders', () => {
-    const {getByText} = render(LangEditor, {lang: 'en', rootUrl: ''})
+    const {getByText} = render(LangEditor, {selectedProject, project})
     const e = getByText(/RAW output/i)
     expect(document.body.contains(e))
   })
 
   it('renders fetched dictionary data correctly', async () => {
     stub(window, 'fetch').resolves({json: () => Promise.resolve(dict)} as Response)
-    const {getByText, container} = render(LangEditor, {lang: 'et', rootUrl: 'i18n'})
-    expect(fetch).calledWith('i18n/en.json')
-    expect(fetch).calledWith('i18n/et.json')
+    const {getByText, container} = render(LangEditor, {selectedProject, project})
+    expect(fetch)
+    expect(fetch)
     await act(fetch)
     await act(fetch)
-    expect(container.querySelectorAll('tbody tr'))
-      .to.have.length(2, 'correct amount of table rows')
     expect(container.querySelector('#rawOutput'))
       .property('value')
       .contains(JSON.stringify(dict, null, 2), 'correct raw output')

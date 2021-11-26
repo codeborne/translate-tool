1e3dcdad650ec3782676029b4ae08e7c1386b2a5
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-26 10:13:12 +0200

    Add jump to top button

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,7 +8,7 @@
 
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-white outline">
+<nav id="top" class="navbar navbar-expand-lg navbar-light bg-white outline">
   <div class="container-fluid">
     <a class="navbar-brand text-dark" href="#"><h3>Translation Tool <i class="fas fa-globe"></i></h3></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">

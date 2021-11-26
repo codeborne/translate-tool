b543d8a9d4311e7dab16c7341ae88ca862626a76
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-26 16:13:48 +0200

    #180444539 Readjust LangSwticher.test.ts

--- src/components/LangSwticher.test.ts
+++ src/components/LangSwticher.test.ts
@@ -3,15 +3,16 @@ import {expect} from 'chai'
 import LangSwitcher from './LangSwitcher.svelte'
 
 describe('<LangSwitcher>', () => {
-  const langs = ['en', 'et', 'es', 'de', 'ru', 'se']
+  const project = {langs:['de', 'et', 'es', 'en', 'ru', 'se'], title:'project', defaultLang: 'de'}
+  const selectedProject = 'project'
 
   it('renders list', () => {
-    const {container} = render(LangSwitcher, {lang: 'de', langs, changed: true})
+    const {container} = render(LangSwitcher, {selectedProject, lang: 'de', project, changed: true})
     expect(container.querySelector('select'))
   })
 
   it('renders list as defined by available languages', () => {
-    const {container} = render(LangSwitcher, {lang: 'de', langs, changed: true})
+    const {container} = render(LangSwitcher, {selectedProject, lang: 'de', project, changed: true})
     expect(container.querySelectorAll('select option')).to.have.length(6)
     expect(container.querySelector('select')!.value).contains('de')
     expect(container.querySelector('select')!.textContent).contains('EN')

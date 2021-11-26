4f5c71c4c02cb3a3e570d4a21d16f3f89478c8ea
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-11-26 15:58:33 +0200

    #180444539 Readjust languageStats.test.ts

--- src/components/languageStats.test.ts
+++ src/components/languageStats.test.ts
@@ -1,19 +1,19 @@
-import {getTotalDictCount, getFilledDictCount} from './languageStats'
+import {getTotalKeys, getTotalFilledKeys} from './languageStats'
 import {expect} from 'chai'
 
-describe('getTotalDictAmount', () => {
+describe('getTotalKeys', () => {
   it('returns correct total count', () => {
-    const dict = getTotalDictCount({
+    const dict = getTotalKeys({
       t1: 1, t2: 2, t: {t3: 3, t4: 4, t: {t5: 5, t6: 6}}})
     expect(dict).to.deep.equal(6)
-    expect(getTotalDictCount({})).to.deep.equal(0)
-    expect(getTotalDictCount({hello: 'world', nested: {hello: 'Another World'}})).to.deep.equal(2)
+    expect(getTotalKeys({})).to.deep.equal(0)
+    expect(getTotalKeys({hello: 'world', nested: {hello: 'Another World'}})).to.deep.equal(2)
   })
 })
 
-describe('getFilledDictCount', () => {
+describe('getTotalFilledKeys', () => {
   it('returns correct total count with filled fields', () => {
-    const dict = getFilledDictCount({
+    const dict = getTotalFilledKeys({
       "1": 1, "2": 2, "obj1": {"3": 3, "4": '', "obj2": {"5": '', "6": ''}}})
     expect(dict).to.deep.equal(3)
   })

6f82d0f43484fc197030f9069c13d7751d064b47
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-03 15:15:00 +0200

    update LangSwitcherTest

--- src/components/LangSwticher.test.ts
+++ src/components/LangSwticher.test.ts
@@ -3,16 +3,22 @@ import {expect} from 'chai'
 import LangSwitcher from './LangSwitcher.svelte'
 
 describe('<LangSwitcher>', () => {
-  const project = {langs:['de', 'et', 'es', 'en', 'ru', 'se'], title:'project', defaultLang: 'de'}
-  const selectedProject = 'project'
+  const langs = ['de', 'et', 'es', 'en', 'ru', 'se']
+  const project = {title: 'project', indent: 2, url: '../../i18n/langs.json'}
+  const lang = 'de'
+  const selectedProjectTitle = 'project'
 
   it('renders list', () => {
-    const {container} = render(LangSwitcher, {selectedProject, lang: 'de', project, changed: true})
+    const {container} = render(LangSwitcher, {selectedProjectTitle,
+      lang,
+      langs,
+      project,
+      changed: true})
     expect(container.querySelector('select'))
   })
 
   it('renders list as defined by available languages', () => {
-    const {container} = render(LangSwitcher, {selectedProject, lang: 'de', project, changed: true})
+    const {container} = render(LangSwitcher, {selectedProjectTitle, lang: 'de', project, changed: true, langs})
     expect(container.querySelectorAll('select option')).to.have.length(6)
     expect(container.querySelector('select')!.value).contains('de')
     expect(container.querySelector('select')!.textContent).contains('EN')

7a4a00934695b3b0bc11c6497e0cfe6f9969e337
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-06 12:24:23 +0200

    Remove comment from ProjectDictionary type

--- src/ProjectDictionary.ts
+++ src/ProjectDictionary.ts
@@ -7,33 +7,3 @@ export interface ProjectDictionaryInfo {
   lang: string,
   dict: Record<string, any>
 }
-
-/*
-[
-  {
-  title: "proj1"
-  dictionaries: [
-    {
-      lang: "en",
-      dict: {},
-    {
-      lang: "de",
-      dict: {}
-      ]
-  },
-  {
-  title: "proj2"
-  projects: [
-    {
-      lang: "et",
-      dict: {
-      app: "eeett"
-    },
-    {
-      lang: "es",
-      dict: {
-      app: "me gusta"
-    }]
-  }
-]
- */

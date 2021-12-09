75e43f8226d8f8267cdd934b8f2f5755618d9800
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-09 15:23:05 +0200

    #180148115 disable the button while saving is in progress

--- src/github/GitHubOutput.svelte
+++ src/github/GitHubOutput.svelte
@@ -6,13 +6,18 @@
   export let lang: string
   export let config: Project
 
+  let inProgress = false
   const client = new GitHubClient(config)
 
   async function save() {
+    inProgress = true
     const result = await client.saveFile(lang, dict)
+    inProgress = false
     if (confirm(`Saved to ${client.branch}, open it for review?`))
       window.open(result.commit.html_url, '_blank')
   }
 </script>
 
-<button on:click={save} class="btn btn-primary w-auto"><i class="fas fa-save me-1"></i> Save to <i>{client.branch}</i> branch</button>
+<button on:click={save} class="btn btn-primary w-auto" disabled={inProgress}>
+  <i class="fas fa-save me-1"></i> Save to <i>{client.branch}</i> branch
+</button>

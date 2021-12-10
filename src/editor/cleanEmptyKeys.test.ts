0b74b11ff76c38e230297b8c864aa7b96ed0a448
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-10 16:25:44 +0200

    Add additional test case for cleanEmptyKeys.test.ts

--- src/editor/cleanEmptyKeys.test.ts
+++ src/editor/cleanEmptyKeys.test.ts
@@ -3,8 +3,38 @@ import {expect} from 'chai'
 
 describe('cleanEmptyKeys', () => {
   it('removes keys with empty values', () => {
-    const dict = cleanEmptyKeys({hello: {empty: '', notEmpty: '123'}})
+    let dict = cleanEmptyKeys({hello: {empty: '', notEmpty: '123'}})
     expect(dict).to.deep.equal({hello: {notEmpty: '123'}})
+    dict = cleanEmptyKeys({"consents": "wee",
+      "contacts": {},
+      "currencySymbols": {},
+      "general": {
+        "filter": {}
+      },
+      "forbidden": {
+        "forbidden1": {},
+        "forbidden2": {},
+        "forbidden3": {
+          "nested1": {},
+          "nested2": {},
+          "nested3": {
+            "more1": {},
+            "more2": {},
+            "more3": {}
+          }
+        }
+      },
+      "notfound": {},
+      "person": {},
+      "products": {
+        "mainParams": {},
+        "params": {},
+        "availability": {}
+      },
+      "validation": {}
+    })
+
+    expect(dict).to.deep.equal({consents: 'wee'})
   })
 
   it('removes empty objects', () => {

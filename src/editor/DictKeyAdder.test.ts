91774c611964a4f0aa36f2c56a8855fc71a8ee27
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-20 15:15:44 +0200

    #180475279 fix duplication in DictKeyAdder test

--- src/editor/DictKeyAdder.test.ts
+++ src/editor/DictKeyAdder.test.ts
@@ -8,24 +8,24 @@ describe('DictKeyAdder', () => {
   const keyPrefix = 'aaa.bbb'
   let dict: Dict = {aaa: {bbb: {ccc: 'cc'}, bb: 'bb'}}
 
-  it('user can cancel', async () => {
+  async function clickPlusButton(promptResult: string|null) {
     const {container} = render(DictKeyAdder, {dict, keyPrefix})
     const button = container.querySelector('button')!
-    stub(window, 'prompt').returns(null)
+    stub(window, 'prompt').returns(promptResult)
     const onChange = fake()
     button.addEventListener('change', onChange)
     await fireEvent.click(button)
+    return onChange
+  }
+
+  it('user can cancel', async () => {
+    const onChange = await clickPlusButton(null)
     expect(prompt).called
     expect(onChange).not.called
   })
 
   it('user can create a new key', async () => {
-    const {container} = render(DictKeyAdder, {dict, keyPrefix})
-    stub(window, 'prompt').returns('new')
-    const button = container.querySelector('button')!
-    const onChange = fake()
-    button.addEventListener('change', onChange)
-    await fireEvent.click(button)
+    const onChange = await clickPlusButton('new')
     expect(prompt).calledWith('New key: aaa.bbb.')
     expect(dict['new']).to.eq('')
     expect(onChange).called

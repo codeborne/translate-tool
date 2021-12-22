c02eb7e91adf2e6c6a77ae44a667884830909622
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-22 10:58:18 +0200

    Introduce insertKey function test

--- src/common/utils.test.ts
+++ src/common/utils.test.ts
@@ -1,5 +1,6 @@
-import {decodeBase64Unicode, deepEqual, encodeBase64Unicode} from './utils'
+import {decodeBase64Unicode, deepEqual, encodeBase64Unicode, insertKey} from './utils'
 import {expect} from 'chai'
+import type {Dict} from './Project'
 
 describe('areObjectsEqual', () => {
   it('returns true if objects are equal, false if not', () => {
@@ -31,7 +32,26 @@ describe('areObjectsEqual', () => {
   })
 })
 
-it('encodes and decodes UTF-8 base64', () => {
-  expect(encodeBase64Unicode('õun')).to.eq('w7V1bg==')
-  expect(decodeBase64Unicode('w7V1bg==')).to.eq('õun')
+describe('encode/decodeBase64Unicode', () => {
+  it('encodes and decodes UTF-8 base64', () => {
+    expect(encodeBase64Unicode('õun')).to.eq('w7V1bg==')
+    expect(decodeBase64Unicode('w7V1bg==')).to.eq('õun')
+  })
+})
+
+describe('insertKey', () => {
+  const keySuffix: string = 'newKey'
+  it('adds a new key in specific order in the object root and preserves nested objects', () => {
+    const before: Dict = {t1: '1', t2: '2', t: {t3: '3', t4: '4', t: {t5: '5', t6: '6'}}}
+    const after: Dict = {t1: '1', newKey: '', t2: '2', t: {t3: '3', t4: '4', t: {t5: '5', t6: '6'}}}
+    expect(insertKey(before, keySuffix, 0)).to.deep.equal(after)
+  })
+
+  it('add a new key in specific order to an existing object child element', () => {
+    let before: Dict = {t1: '1', t2: '2', t: {t3: '3', t4: '4', t: {t5: '5', t6: '6'}}}
+    const after: Dict = {t1: '1', t2: '2', t: {t3: '3', t4: '4', newKey: '', t: {t5: '5', t6: '6'}}}
+
+    before['t'] = insertKey(before['t'] as Dict, keySuffix, 1)
+    expect(before).to.deep.equal(after)
+  })
 })

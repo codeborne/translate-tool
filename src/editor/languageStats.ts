da391498a6b55a8e070199812b533a18b06cff53
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-22 12:32:26 +0200

    #180584910 Add function which counts how many keys are different in when two objects are compared

--- src/editor/languageStats.ts
+++ src/editor/languageStats.ts
@@ -3,10 +3,19 @@ import type {Dict} from '../common/Project'
 export function totalKeys(dict: Dict) {
   let total = 0
   for (let value of Object.values(dict)) {
-    // total++
     const isObject = typeof value === 'object'
     if (isObject) total += totalKeys(value as Dict)
     if (value && !isObject) total++
   }
   return total
+}
+
+export function totalDifferentValues(origDict: Dict, modifiedDict: Dict) {
+  let changes = 0
+  for (let [key, value] of Object.entries(origDict)) {
+    const isObject = typeof value === 'object'
+    if (isObject) changes += totalDifferentValues(value as Dict, modifiedDict[key] as Dict)
+    if (!isObject && value !== modifiedDict[key]) changes++
+  }
+  return changes
 }

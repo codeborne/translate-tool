0bea5c15733d04ff771831f34f8c7a870027f077
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-22 15:39:22 +0200

    Add ChangesCounter test

--- src/editor/ChangesCounter.test.ts
+++ src/editor/ChangesCounter.test.ts
@@ -3,13 +3,16 @@ import {expect} from 'chai'
 import ChangesCounter from './ChangesCounter.svelte'
 
 describe('ChangesCounter', () => {
-
   const uneditedDict = {t1: '1', t2: '2', t: {t3: '3', t4: '4', t: {t5: '5', t6: '6'}}}
-  const dict = {t1: 'new', t2: '2', t: {t3: '3', t4: '4', t: {t5: '', t6: 'new value'}}}
+  let dict = uneditedDict
+  it('Shows default value upon render', async () => {
+    const {container, component} = render(ChangesCounter, {dict, uneditedDict})
+    expect(container.querySelector('div span')!.textContent).to.contain('0')
+  })
 
-  it('renders', async () => {
-    const {container, getByText} = render(ChangesCounter, {dict, uneditedDict})
-    const e = getByText(/0/)
-    expect(container).to.contain(e)
+  it('Shows correct amount of different values if dictionaries are different', async () => {
+    dict = {t1: 'new', t2: '2', t: {t3: '3', t4: '4', t: {t5: '', t6: 'new value'}}}
+    const {container, component} = render(ChangesCounter, {dict, uneditedDict})
+    expect(container.querySelector('div span')!.textContent).to.contain('3')
   })
 })

8b1b446934fad4f9b9451a065bd7d58a3497566e
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-22 15:40:07 +0200

    #180584910 Adjust style of ChangesCounter with fading and different icon color

--- src/editor/ChangesCounter.svelte
+++ src/editor/ChangesCounter.svelte
@@ -3,24 +3,25 @@
 
   export let dict
   export let uneditedDict
+
   let changes = 0
 
   $: if (dict) changes = totalDifferentValues(dict, uneditedDict)
 </script>
 
-<div class="card flex-row justify-content-center bg-light align-items-center p-1 gap-2 text-secondary mt-2 mb-2" class:invisible={!changes}>
-  <i class="fas fa-info-circle text-primary"></i>
-  <span class="text-secondary"><span id="changeCounter">{changes} changes</span></span>
+<div class="card flex-row justify-content-center bg-light align-items-center p-1 gap-2 text-secondary mt-2 mb-2"
+     class:invisible={!changes}>
+  <i class="fas fa-info-circle text-warning"></i>
+  <span>{changes} changes</span>
 </div>
 
 <style>
   .text-secondary {
     font-size: 0.95em;
-    transition: 0.8s;
+    transition: 0.6s;
   }
   .invisible {
     visibility: hidden;
     opacity: 0;
-    transition: 0.8s;
   }
 </style>

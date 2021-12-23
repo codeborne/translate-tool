4187ab16df1e41b095e5cbc528a810e2990480c3
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-23 13:20:51 +0200

    #180584936 User's last language is saved to localstorage

--- src/layout/LangSwitcher.svelte
+++ src/layout/LangSwitcher.svelte
@@ -2,9 +2,15 @@
   import type {LoadedProject} from '../common/Project'
 
   export let project: LoadedProject
-  export let lang: string = project.langs[0]
+  export let lang: string = localStorage.getItem('selectedLanguage') ?? project.langs[0]
 
-  $: if (!project.dicts[lang]) lang = project.langs[0]
+  $: if (!project.dicts[lang]) {
+    lang = project.langs[0]
+    localStorage.setItem('selectedLanguage', lang)
+  }
+
+  $: if (lang) localStorage.setItem('selectedLanguage', lang)
+
 </script>
 
 <select bind:value={lang} class="form-select w-auto me-3">

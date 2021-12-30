ac7907959947601f903989c4a4dc8432897906da
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-30 11:40:06 +0200

    #180585047 Add rendering and input tests in TextInput component

--- src/editor/TextInput.test.ts
+++ src/editor/TextInput.test.ts
@@ -1,15 +1,30 @@
 import {render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import TextInput from './TextInput.svelte'
+import type {Dict} from '../common/Project'
 
 describe('TextInput', () => {
+
+  const dict: Dict = {something: 'no html', somethingHtml: 'html', b: {c: 'c'}}
+  let key = 'somethingHtml'
+  let lang = 'en'
+
   it('renders editable div if given key that ends with Html', async () => {
-    const {container} = render(TextInput)
-    expect(container.textContent).to.contain('TODO')
+    const {container} = render(TextInput, {uneditedDict:dict, dict, key, lang})
+    const element = container.querySelector('[contenteditable]')
+    expect(element).to.exist
+    expect(element!.textContent).to.contain('html')
+    element!.textContent = 'edited'
+    expect(element!.textContent).to.contain('edited')
   })
 
   it('renders textarea if key does not end with Html', async () => {
-    const {container} = render(TextInput)
-    expect(container.textContent).to.contain('TODO')
+    key = 'something'
+    const {container, component} = render(TextInput, {uneditedDict:dict, dict, key, lang})
+    const element = container.querySelector('textarea')
+    expect(element).to.exist
+    expect(element!.value).to.contain('html')
+    element!.value = 'edited'
+    expect(element!.value).to.contain('edited')
   })
 })

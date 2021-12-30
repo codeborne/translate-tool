33b1181fec5bfe70fce49dd5b072484b03bb508a
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-30 16:57:11 +0200

    #180585047 Improve tests

--- src/editor/TextInput.test.ts
+++ src/editor/TextInput.test.ts
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/svelte'
+import {fireEvent, render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import TextInput from './TextInput.svelte'
 import type {Dict} from '../common/Project'
@@ -14,8 +14,18 @@ describe('TextInput', () => {
     const element = container.querySelector('[contenteditable]')
     expect(element).to.exist
     expect(element!.textContent).to.contain('html')
+    expect(container.querySelector('.btn')).to.exist
     element!.textContent = 'edited'
     expect(element!.textContent).to.contain('edited')
+  })
+
+  it('Clicking on html button renders the HTML preview', async () => {
+    const {container} = render(TextInput, {uneditedDict:dict, dict, key, lang})
+    const button: HTMLButtonElement|null = container.querySelector('.btn')
+    expect(button).to.exist
+    expect(container.querySelector('.preview')).to.not.exist
+    await fireEvent.click(button!)
+    expect(container.querySelector('.preview')).to.exist
   })
 
   it('renders textarea if key does not end with Html', async () => {
@@ -26,5 +36,6 @@ describe('TextInput', () => {
     expect(element!.value).to.contain('html')
     element!.value = 'edited'
     expect(element!.value).to.contain('edited')
+    expect(container.querySelector('.btn')).to.not.exist
   })
 })

b02c4dd85f5bb21b58857036d9a5895663729333
Markus Tammeoja <markus.tammeoja@gmail.com>
2021-12-31 10:37:56 +0200

    #180767878 Add descriptive title to add/edit project icon

--- src/layout/ProjectAddButton.svelte
+++ src/layout/ProjectAddButton.svelte
@@ -2,6 +2,7 @@
   export let showAddProject: boolean
 </script>
 
-<button on:click={() => showAddProject = !showAddProject} class="btn btn-primary" type="button">
+<button on:click={() => showAddProject = !showAddProject} class="btn btn-primary" type="button"
+        title={showAddProject ? 'Edit current project' : 'Add a new project'}>
   <i class="fas {showAddProject ? 'fa-edit' : 'fa-plus-circle'}"></i>
 </button>

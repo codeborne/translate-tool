5acbb12c917ef8a53e76f53e8c6330d6f9bdbf26
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-03 15:47:01 +0200

    #180444539 Add test for FilterControls.test.ts

--- src/editor/FilterControls.test.ts
+++ src/editor/FilterControls.test.ts
@@ -1,10 +1,16 @@
 import {render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import FilterControls from './FilterControls.svelte'
+import Filter from './Filter'
 
 describe('FilterControls', () => {
-  it('renders', async () => {
-    const {container} = render(FilterControls)
-    expect(container.textContent).to.contain('TODO')
+
+  let filter: Filter = new Filter('test', false)
+
+  it('Shows correct filters on render', async () => {
+    const {container} = render(FilterControls, {filter})
+    expect(container.querySelector('.form-check-label')!.textContent).to.contain('Show only empty')
+    expect((container.querySelector('.form-control') as HTMLInputElement).value).to.equal('test')
+    expect((container.querySelector('.form-check-input') as HTMLInputElement).value).to.equal('on')
   })
 })

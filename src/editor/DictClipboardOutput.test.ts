00d6b899cc22bb861eec32043b4da8ce80d6fecf
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-03 15:13:07 +0200

    #180444539 Add copying test to DictClipboardOutput.test.ts

--- src/editor/DictClipboardOutput.test.ts
+++ src/editor/DictClipboardOutput.test.ts
@@ -1,7 +1,9 @@
-import {render} from '@testing-library/svelte'
+import {fireEvent, render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import DictClipboardOutput from './DictClipboardOutput.svelte'
 import type {Dict} from '../common/Project'
+import {stub} from 'sinon'
+import {tick} from 'svelte'
 
 describe('DictClipboardOutput', () => {
 
@@ -9,8 +11,18 @@ describe('DictClipboardOutput', () => {
   const lang: string = 'en'
   const indent: number = 2
 
-  it('renders', async () => {
-    const {container} = render(DictClipboardOutput)
-    expect(container.textContent).to.contain('TODO')
+  it('shows textarea with no empty keys', async () => {
+    const {container} = render(DictClipboardOutput, {dict, lang, indent})
+    expect(container.querySelector('textarea')!.value).to.contain('{\n  "t1": "t1",\n  "t2": "t2",\n  "t3": "t3"\n}')
+  })
+
+  it('clicking copy copies to clipboard', async () => {
+    const {container} = render(DictClipboardOutput, {dict, lang, indent})
+    const buttonElement = container.querySelector('button') as HTMLButtonElement
+    expect(buttonElement).to.exist
+    let copy = stub(document, 'execCommand')
+    await fireEvent.click(buttonElement)
+    await tick()
+    expect(copy).called
   })
 })

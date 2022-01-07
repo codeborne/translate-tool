6489eef8c5cd6e0f46bea35f569a2d68cce3b882
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-07 15:31:51 +0200

    #180475298 Refactor GoogleAuth.svelte

--- src/common/GoogleAuth.svelte
+++ src/common/GoogleAuth.svelte
@@ -2,16 +2,27 @@
 
   import {onMount} from 'svelte'
   import type {GoogleAuthUser} from './GoogleAuthUser'
+  import jsonLoader from './JsonLoader'
 
   export let user: GoogleAuthUser
 
   let GoogleAuth
   let clientId: string
 
-  onMount(() => {
-    clientId = '530758130588-2smj2veadedmqa5eemrjund6aivhr1al.apps.googleusercontent.com'
-    if (clientId) load()
+  onMount(async () => {
+    await loadAuthInfo()
+    if (clientId) await init()
   })
+
+  async function loadAuthInfo() {
+    const authFile: Record<string, any> = await tryLoadAuthFile()
+    if (!authFile) return
+    clientId = authFile['google']['client_id']
+  }
+
+  async function tryLoadAuthFile() {
+    return jsonLoader.loadJson('auth.json').catch()
+  }
 
   async function handleLogin() {
     await GoogleAuth.signIn()
@@ -20,36 +31,36 @@
 
   function checkForLoggedInUser() {
     if (GoogleAuth.isSignedIn.get()) {
-      user = GoogleAuth.currentUser.get().getBasicProfile();
+      user = GoogleAuth.currentUser.get().getBasicProfile()
     } else {
       user = undefined
     }
   }
 
-  async function load() {
+  async function init() {
     gapi.load('auth2', () => {
       GoogleAuth = gapi.auth2.init({
         client_id: clientId
       })
-      GoogleAuth.then(checkForLoggedInUser);
-    });
+      GoogleAuth.then(checkForLoggedInUser)
+    })
   }
 
   async function handleLogout() {
     let auth = gapi.auth2.getAuthInstance()
     await auth.signOut().then(() => {
       checkForLoggedInUser()
-    });
+    })
   }
 </script>
 
 {#if clientId}
   {#if user}
-    <button class="btn btn-light" on:click={handleLogout}>
+    <button class="btn btn-outline-secondary" on:click={handleLogout}>
       <i class="fab fa-google"></i> Log out
     </button>
   {:else}
-    <button class="btn btn-light" on:click={handleLogin}>
+    <button class="btn btn-outline-secondary" on:click={handleLogin}>
       <i class="fab fa-google"></i> Login
     </button>
   {/if}

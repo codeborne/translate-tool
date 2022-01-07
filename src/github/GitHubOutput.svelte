54457c707a200f953d4b1be510954340bbc8fe73
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-07 14:17:25 +0200

    #180475298 set the logged in user as commit author

--- src/github/GitHubOutput.svelte
+++ src/github/GitHubOutput.svelte
@@ -2,16 +2,19 @@
 
   import type {Dict, Project} from '../common/Project'
   import {GitHubClient} from './GitHubClient'
+  import type {GoogleAuthUser} from '../common/GoogleAuthUser'
 
   export let dict: Dict
   export let lang: string
   export let config: Project
+  export let user: GoogleAuthUser
 
   $: client = new GitHubClient(config)
   let inProgress = false
 
   async function save() {
     inProgress = true
+    checkIfUserExists()
     const commitMessage = prompt('Commit message (what have you changed?)', `Updated ${lang} translations`)
     try {
       if (!commitMessage) return
@@ -20,6 +23,13 @@
         window.open(result.commit.html_url, '_blank')
     } finally {
       inProgress = false
+    }
+  }
+
+  function checkIfUserExists() {
+    if (user) {
+      client.setAuthorEmail(user.getEmail())
+      client.setAuthorName(user.getName())
     }
   }
 </script>

cf26c1c2061742b3658bceef7d1589048374a0f2
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-07 16:13:13 +0200

    #180475298 Add GoogleAuth tests for auth file detection

---
+++ src/common/GoogleAuth.test.ts
@@ -0,0 +1,36 @@
+import {act, render} from '@testing-library/svelte'
+import {expect} from 'chai'
+import GoogleAuth from './GoogleAuth.svelte'
+import type {GoogleAuthUser} from './GoogleAuthUser'
+import {stub} from 'sinon'
+
+describe('GoogleAuth', () => {
+  const user: GoogleAuthUser = undefined
+  const authFile = {google: { client_id: "google_auth_client_id"}}
+
+  beforeEach(() => {
+    // @ts-ignore
+    window['gapi'] = {
+      load() {
+        return null;
+      }
+    }
+  })
+
+  it('renders nothing if auth file contains no client id', async () => {
+    const {container} = render(GoogleAuth, {user})
+    stub(window, 'fetch').resolves(undefined)
+    await act(() => Promise.resolve())
+    expect(container.querySelector('.login')).to.not.exist
+    expect(container.querySelector('.logout')).to.not.exist
+  })
+
+  it('renders login/logout buttons if auth file has client id', async () => {
+    stub(window, 'fetch').resolves({json: async () => authFile} as Response)
+    const {container} = render(GoogleAuth, {user})
+    expect(fetch).called
+    await act(() => Promise.resolve())
+    expect(container.querySelector('.login')).to.exist
+    expect(container.querySelector('.logout')).to.exist
+  })
+})

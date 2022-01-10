38ef9cc4ec1c68cd7379681b56401f11a21fb422
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-10 11:02:24 +0200

    Slightly refactor GitHubProjectImporter.test.ts to remove duplication

--- src/config/GitHubProjectImporter.test.ts
+++ src/config/GitHubProjectImporter.test.ts
@@ -6,6 +6,17 @@ import {stub} from 'sinon'
 import GitHubProjectImporter from './GitHubProjectImporter.svelte'
 import {GitHubClient} from '../github/GitHubClient'
 
+function setInputFields(container: HTMLElement) {
+  const inputs = container.querySelectorAll('input')
+  inputs[0].value = 'projectname'
+  inputs[1].value = 'repoowner'
+  inputs[2].value = 'reponame'
+  inputs[3].value = 'path'
+  inputs[4].value = 'token'
+  expect(inputs[5].value).to.contain('translations')
+  return inputs
+}
+
 describe('GitHubProjectImporter', () => {
   const projects: Project[] = [
     {title:'project', url:'someurl', indent: 2, token:'123123', branch: 'translations'}
@@ -17,12 +28,7 @@ describe('GitHubProjectImporter', () => {
     const button = container.querySelector('button') as HTMLButtonElement
     expect(button.textContent).to.contain('Import')
     expect(container.querySelector('.alert')).to.not.exist
-    const inputs = container.querySelectorAll('input')
-    inputs[0].value = 'projectname'
-    inputs[1].value = 'repoowner'
-    inputs[2].value = 'reponame'
-    inputs[3].value = 'path'
-    inputs[4].value = 'token'
+    const inputs = setInputFields(container)
     let client = new GitHubClient({
       url: `https://api.github.com/repos/${inputs[1].value}/${inputs[2].value}/contents${inputs[3].value}`,
       token: inputs[4].value
@@ -37,13 +43,7 @@ describe('GitHubProjectImporter', () => {
     const {container} = render(GitHubProjectImporter, {projects})
     const button = container.querySelector('button') as HTMLButtonElement
     expect(button.textContent).to.contain('Import')
-    const inputs = container.querySelectorAll('input')
-    inputs[0].value = 'projectname'
-    inputs[1].value = 'repoowner'
-    inputs[2].value = 'reponame'
-    inputs[3].value = '/i18n/'
-    inputs[4].value = 'token'
-
+    const inputs = setInputFields(container)
     let client = new GitHubClient({
       url: `https://api.github.com/repos/${inputs[1].value}/${inputs[2].value}/contents${inputs[3].value}`,
       token: inputs[4].value

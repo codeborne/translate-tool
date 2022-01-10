8eeac1ed0e9ac85ce9ccf2b1436cdc61939356de
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-10 14:37:36 +0200

    #180767939 Add public dictionary importing E2E tests

--- e2e/import.test.js
+++ e2e/import.test.js
@@ -1,13 +1,18 @@
 import {expect, test} from '@playwright/test'
 import {url} from './config.js'
 
-test('import', async ({page}) => {
+async function fillPublicImport(projectName, page) {
+  await expect(page.locator('.collapsePublic')).toBeVisible()
+  await page.locator('.collapsePublic input:nth-of-type(1)').fill(projectName)
+  await page.locator('.collapsePublic input:nth-of-type(2)').fill('https://raw.githubusercontent.com/Uptaker/i18n-public/main/i18n/')
+}
+
+test('import public twice', async ({page}) => {
   await page.goto(url)
   await expect(page.locator('#top h3')).toContainText('Translation Tool')
-  await expect(page.locator('.collapsePublic')).toBeVisible()
-  await page.locator('.collapsePublic input:nth-of-type(1)').fill('E2E')
-  await page.locator('.collapsePublic input:nth-of-type(2)').fill('https://raw.githubusercontent.com/Uptaker/i18n-public/main/i18n/')
+  await fillPublicImport("E2E", page)
   await page.locator('.collapsePublic button').click()
+  await expect(page.locator('.collapsePublic')).not.toBeVisible()
 
   await expect(page.locator('#output')).toBeVisible()
   await expect(page.locator('#backToImporterBtn')).toBeVisible()
@@ -16,4 +21,14 @@ test('import', async ({page}) => {
   await expect(page.locator('nav .nav-link')).toBeVisible()
   await expect(page.locator('nav .nav-link:first-of-type')).toContainText('E2E')
   await expect(page.locator('nav .nav-link:nth-of-type(2)')).not.toBeVisible()
+
+  await page.locator('[title="Add a new project"]').click()
+  await expect(page.locator('.collapsePublic')).toBeVisible()
+  await fillPublicImport("OtherDict", page)
+  await page.locator('.collapsePublic button').click()
+  await expect(page.locator('.collapsePublic')).not.toBeVisible()
+  await expect(page.locator('#output')).toBeVisible()
+  await expect(page.locator('nav .nav-link:nth-of-type(1)')).toContainText('E2E')
+  await expect(page.locator('nav .nav-link:nth-of-type(2)')).toContainText('OtherDict')
+  await expect(page.locator('nav .nav-link:nth-of-type(3)')).not.toBeVisible()
 })

b1c531d433718c201c093a9eed912a99c8a793ab
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-10 11:48:01 +0200

    #180767805 Add tests for BranchLoadedFrom.test.ts

--- src/editor/BranchLoadedFrom.test.ts
+++ src/editor/BranchLoadedFrom.test.ts
@@ -4,9 +4,26 @@ import BranchLoadedFrom from './BranchLoadedFrom.svelte'
 import type {Project} from '../common/Project'
 
 describe('BranchLoadedFrom', () => {
-  let config: Project = {url: 'url', token: 'token', indent: 2, title: 'title'}
+  let config: Project = {url: 'api.github.com', token: 'token', indent: 2, title: 'title'}
+
   it('renders default branch if no branch in config', async () => {
     const {container} = render(BranchLoadedFrom, { config })
-    expect(container.querySelector('div')).to.contain('TODO')
+    const text = container.querySelector('div')?.textContent
+    console.log(container.querySelector('div'))
+    expect(text).to.contain('Translations loaded from translations branch')
+  })
+
+  it('renders custom branch from config', async () => {
+    config.branch = 'test'
+    const {container} = render(BranchLoadedFrom, { config })
+    const text = container.querySelector('div')?.textContent
+    console.log(container.querySelector('div'))
+    expect(text).to.contain('Translations loaded from test branch')
+  })
+
+  it('does not render if not a github project', async () => {
+    config.url = 'not.github.url'
+    const {container} = render(BranchLoadedFrom, { config })
+    expect(container.querySelector('.form-text')).to.not.exist
   })
 })

af4b2d307dd08afd3a46ad297a6f9696c0501491
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-10 15:41:06 +0200

    #180767939 Add change counter tests & counter after saving

--- e2e/editor.test.js
+++ e2e/editor.test.js
@@ -9,10 +9,11 @@ async function fillPublicImport(page) {
   await expect(page.locator('.collapsePublic')).not.toBeVisible()
 }
 
-test('editor', async ({page}) => {
+test('editor page functionality', async ({page}) => {
   await page.goto(url)
   await expect(page.locator('#top h3')).toContainText('Translation Tool')
   await fillPublicImport(page)
+  await expect(page.locator('.num-changes')).not.toBeVisible()
   await expect(page.locator('nav .nav-link:first-of-type')).toContainText('EditorTest')
   expect(await page.inputValue('table .form-control:first-of-type')).toContain('Public Testing')
   expect(await page.inputValue('#rawOutput')).toContain('Public Testing')
@@ -22,5 +23,23 @@ test('editor', async ({page}) => {
   expect(await page.inputValue('table .form-control:first-of-type')).toContain('Changed! Abcdefghijklmnopqrstuvwxyz')
   expect(await page.inputValue('#rawOutput')).toContain('Changed! Abcdefghijklmnopqrstuvwxyz')
   expect(await page.inputValue('#rawOutput')).not.toContain('Public Testing')
+
+  await expect(page.locator('.num-changes')).toBeVisible()
+  await expect(page.locator('.num-changes')).toContainText('1 change')
+  await page.locator('table .form-control').last().fill('other change')
+  await page.locator('[href="#output"]').click()
+  await expect(page.locator('.num-changes')).toContainText('2 changes')
+  await page.locator('table .form-control').last().fill('Submit')
+  await page.locator('table .form-control').first().fill('Public Testing')
+  await page.locator('[href="#output"]').click()
+  await expect(page.locator('.num-changes')).not.toBeVisible()
+
+  await page.locator('table .form-control').last().fill('to be saved')
+  await page.locator('[href="#output"]').click()
+  await expect(page.locator('.num-changes')).toContainText('1 change')
+  expect(await page.inputValue('#rawOutput')).toContain('to be saved')
+  await page.locator('text=clipboard').click()
+  await page.on('dialog', dialog => dialog.accept())
+  await expect(page.locator('.num-changes')).not.toBeVisible()
 })
 

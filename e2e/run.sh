3c795eb25455e87af54a71af8b36e437394dc181
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-14 10:59:30 +0200

    move development projects.json out of the way for tests

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-set -e
 
 cd `dirname $0`
 
 npm ci
 
 cd ../build
+mv projects.json projects.json.bak 2>/dev/null
 npx -y http-server -p 9874 &
 SERVER_PID=$!
 
 cd -
 TEST_URL='http://localhost:9874' npm test
+RESULT=$?
 kill $SERVER_PID
+
+cd ../build
+mv projects.json.bak projects.json 2>/dev/null
+exit $RESULT

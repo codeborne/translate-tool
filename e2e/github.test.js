54fdf2b6a08108ac20d65a19353e26bbcd8b9fc4
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-14 12:44:00 +0200

    #180767939 Add GitHub commit e2e test

--- e2e/github.test.js
+++ e2e/github.test.js
@@ -1,8 +1,17 @@
 import {expect, test} from '@playwright/test'
-import {github_project, url} from './config.js'
+import {githubProject, url} from './config.js'
+
+async function handlePrompt(dialog) {
+  await expect(dialog.message).toBe('Commit message (what have you changed?)')
+  await dialog.accept(`Commit from E2E tests on ${new Date().toLocaleString()}`)
+}
 
 test('can commit to github', async ({page}) => {
-  await page.goto(`${url}/?shared=${github_project}`)
+  await page.goto(`${url}/?shared=${githubProject}`)
   await expect(page.locator('#top h3')).toContainText('Translation Tool')
   await expect(page.locator('nav .nav-link:first-of-type')).toContainText('TestProject')
+  await expect(page.locator('text=Save to translations branch')).toBeVisible()
+  await page.once('dialog', dialog => dialog.accept(`Commit from E2E tests on ${new Date().toLocaleString()}`))
+  await page.locator('text=Save to translations branch').click()
+  await page.once('dialog', dialog => dialog.accept())
 })

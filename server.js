a383be479ac225bc6fd5fcd991c857a1e06b7816
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-17 11:17:22 +0200

    #180872753 Add configurable port where the application would run in

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express'),
   app = express()
 const request = require('request')
 
+const port = process.env.PORT ?? 8998
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
@@ -11,4 +13,4 @@ app.get('/**', (req, res) => {
   const url = req.url.slice(1, req.url.length)
   request(url).pipe(res)
 })
-app.listen(8998, () => console.log(`Listening on http://localhost:8998`));
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`))

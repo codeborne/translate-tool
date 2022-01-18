f56018e55cf8f983b790e1e2ab71d829fc44cb74
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-18 13:04:33 +0200

    #180935187 BitBucketClient can now fetch access tokens and read private repositories

--- src/bitbucket/BitBucketClient.ts
+++ src/bitbucket/BitBucketClient.ts
@@ -1,8 +1,5 @@
-import type {Dict, Project} from '../common/Project'
-import {LoadedProject} from '../common/Project'
-import {encodeBase64Unicode} from '../common/utils'
+import type {Project} from '../common/Project'
 import jsonLoader from '../common/JsonLoader'
-import {cleanEmptyKeys} from '../editor/cleanEmptyKeys'
 
 export class BitBucketClient {
   static host = 'api.bitbucket.org'
@@ -21,16 +18,20 @@ export class BitBucketClient {
     this.author.email = email
   }
 
-  authHeader() {
-    return this.config.token ? {Authorization: 'token ' + this.config.token} : undefined
+  basicAuthHeader() {
+    return this.config.token ? {Authorization: 'Basic ' + this.config.token} : undefined
+  }
+
+  tokenHeader(token: string|undefined) {
+    return token ? {Authorization: 'Bearer ' + token} : undefined
   }
 
   request(url: string, init?: RequestInit) {
-    return jsonLoader.request(url, {...init, headers: {...this.authHeader(), ...init?.headers}})
+    return jsonLoader.request(url, {...init, headers: {...init?.headers}})
   }
 
   send(url: string, method: string, body: any) {
-    return this.request(url, {method, body: JSON.stringify(body), headers: {'Content-Type': 'application/json', 'Accept': 'application/vnd.github.v3+json'}})
+    return this.request(url, {method, body, headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
   }
 
   post(url: string, body: any) {
@@ -41,56 +42,31 @@ export class BitBucketClient {
     return this.send(url, 'PUT', body)
   }
 
+  async getAccessToken(url: string) {
+    if (!this.config.token) return undefined
+    const split = this.config.token.split(':')
+    const body = 'grant_type=client_credentials&client_id=' + split[0] + '&client_secret=' + split[1]
+    return await this.post('https://bitbucket.org/site/oauth2/access_token', body) as BitBucketAuthResponse
+  }
 
 
   async getFile(file: string, branch?: string) {
     const url = branch ? this.config.url.replace('/main/', `/${branch}/`) : this.config.url
-    return await this.request(url + file)
+    const token = await this.getAccessToken(url + file)
+    return await this.fetchFile(url + file, token?.access_token)
   }
 
-  async saveFile(lang: string, dict: Dict, commitMessage: string) {
-    await this.createBranchIfNeeded()
-    const fileName = lang + '.json'
-    const content = encodeBase64Unicode(LoadedProject.prettyFormat(cleanEmptyKeys(dict), this.config.indent))
-    const previousFileBlobSha = (await this.getFile(fileName + '?ref=' + this.branch)).sha
-    return await this.put(this.config.url + fileName, {
-      message: commitMessage,
-      sha: previousFileBlobSha,
-      branch: this.branch,
-      content,
-      author: this.author
-    }) as BitBucketSavedFile
+  async fetchFile(url: string,  token: string|undefined, init?: RequestInit,) {
+    return await jsonLoader.request(url, {...init, headers: {...this.tokenHeader(token), ...init?.headers}})
   }
 
-  private async createBranchIfNeeded() {
-    const refsUrl = this.config.url.replace(/contents\/.*$/, 'git/refs')
-    const refs = await this.request(refsUrl) as BitBucketRef[]
-
-    let branchSha = refs.find(r => r.ref == 'refs/heads/' + this.branch)?.object.sha
-    if (!branchSha) {
-      branchSha = refs[0].object.sha
-      await this.post(refsUrl, {ref: 'refs/heads/' + this.branch, sha: branchSha})
-    }
-  }
 }
 
-export interface BitBucketFile {
-  content: string,
-  encoding: string,
-  sha: string
+export interface BitBucketAuthResponse {
+  access_token: string,
+  expires_in: number,
+  refresh_token: string,
+  scopes: string,
+  token_type: string
 }
 
-export interface BitBucketSavedFile {
-  content: {name: string, path: string, sha: string, html_url: string}
-  commit: {sha: string, html_url: string}
-}
-
-export interface BitBucketRef {
-  ref: string,
-  url: string
-  object: {
-    type: string,
-    sha: string,
-    url: string
-  }
-}

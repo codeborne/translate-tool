8863cd8b2242b59203d1bab3bcee0fd9a8750603
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-19 17:09:21 +0200

    #180959682 Redo BranchLoadedFrom.svelte to show the actual loaded branch by checking whether a request for a file from that branch was successful or not

--- src/editor/BranchLoadedFrom.svelte
+++ src/editor/BranchLoadedFrom.svelte
@@ -1,10 +1,33 @@
 <script>
+  import {GitHubClient} from '../github/GitHubClient'
+  import {BitBucketClient} from '../bitbucket/BitBucketClient'
+
   export let config
+
+  let branch = config.branch
+
+  $: if (config) isVersionControl()
+
+  async function isVersionControl() {
+    try {
+      let result
+      if (config.url.includes(GitHubClient.host)) result =
+        await new GitHubClient(config).getFileContentNoCatch('langs.json')
+      else if (config.url.includes(BitBucketClient.host)) result =
+        await new BitBucketClient(config).getFileNoCatch('langs.json', config.branch ?? 'translations')
+      if (result) branch = config.branch ?? 'translations'
+      else branch = 'main'
+    } catch (e) {
+      branch = 'main'
+      console.warn(`The project ${config.title} does not have a branch called ${config.branch ?? 'translations'}.`)
+    }
+  }
+
 </script>
 
-{#if config.url.includes('api.github.com')}
+{#if branch}
   <div class="form-text">
     <i class="fas fa-code-branch"></i>
-    Translations loaded from <b>{config.branch ? config.branch : 'translations'}</b> branch
+    Translations loaded from <b>{branch}</b> branch
   </div>
 {/if}

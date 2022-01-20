aa70d806a596d5e737d6b0ccec87f8e8d681a4e9
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-20 10:38:39 +0200

    #180959682 Attempts to fix test, but I ran into request testing issues once again - added TODO

--- src/editor/BranchLoadedFrom.test.ts
+++ src/editor/BranchLoadedFrom.test.ts
@@ -2,17 +2,24 @@ import {render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import BranchLoadedFrom from './BranchLoadedFrom.svelte'
 import type {Project} from '../common/Project'
+import {GitHubClient} from '../github/GitHubClient'
+import {stub} from 'sinon'
 
 describe('BranchLoadedFrom', () => {
-  let config: Project = {url: 'api.github.com', token: 'token', indent: 2, title: 'title'}
+  let config: Project = {url: 'https://api.github.com/repos/org/repo/contents/i18n/', token: 'token', indent: 2, title: 'title'}
 
-  it.skip('renders default branch if no branch in config', async () => {
+  const client = new GitHubClient(config)
+
+  it.skip('renders main branch if it does not exist or have any contents', async () => {
+    stub(client, 'getFileContentNoCatch').resolves('value that is not empty')
     const {container} = render(BranchLoadedFrom, { config })
+    expect(client.getFileContentNoCatch).called // TODO consult on why this doesn't work
+    // await act(client.getFileContentNoCatch)
     const text = container.querySelector('div')?.textContent
-    expect(text).to.contain('Translations loaded from translations branch')
+    expect(text).to.contain('Translations loaded from main branch')
   })
 
-  it.skip('renders custom branch from config', async () => {
+  it.skip('renders custom/translations branch from config', async () => {
     config.branch = 'test'
     const {container} = render(BranchLoadedFrom, { config })
     const text = container.querySelector('div')?.textContent

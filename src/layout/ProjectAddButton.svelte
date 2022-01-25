abb62091dfa4344ca6480d175e0ffe706becb919
Ian Syalik <ian@codeborne.com>
2022-01-25 16:51:52 +0200

    add label to button

--- src/layout/ProjectAddButton.svelte
+++ src/layout/ProjectAddButton.svelte
@@ -5,6 +5,6 @@
 {#if !showAddProject}
   <button on:click={() => showAddProject = !showAddProject} class="btn btn-primary" type="button"
           title="Add a new project">
-    <i class="fas fa-plus-circle"></i>
+    <i class="me-2 fas fa-plus-circle"></i>Add a new project
   </button>
 {/if}

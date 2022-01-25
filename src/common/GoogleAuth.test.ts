97baa1f9bc0e0e7892ce5953169b5bdfa164bc56
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-25 15:29:28 +0200

    Remove most of GoogleAuth test in & skip placeholder until implemented

--- src/common/GoogleAuth.test.ts
+++ src/common/GoogleAuth.test.ts
@@ -7,32 +7,12 @@ import jsonLoader from './JsonLoader'
 
 describe('GoogleAuth', () => {
   const user: GoogleAuthUser = undefined
-  const authFile = {google: { client_id: "google_auth_client_id"}}
 
-  beforeEach(() => {
-    // @ts-ignore
-    window['gapi'] = {
-      load() {
-        return null
-      },
-    }
-  })
-
-  it('renders nothing if auth file contains no client id', async () => {
+  it.skip('renders if a user is found', async () => {
     stub(jsonLoader, 'loadJson').resolves(undefined)
     const {container} = render(GoogleAuth, {user})
     expect(jsonLoader.loadJson).called
     await act(jsonLoader.loadJson)
-    expect(container.querySelector('.login')).to.not.exist
-    expect(container.querySelector('.logout')).to.not.exist
-  })
-
-  it.skip('renders login/logout buttons if auth file has client id', async () => {
-    stub(jsonLoader, 'loadJson').resolves({json: async () => authFile} as Response)
-    const {container} = render(GoogleAuth, {user})
-    expect(jsonLoader.loadJson).calledWith('auth.json')
-    await act(jsonLoader.loadJson)
-    expect(container.querySelector('.login')).to.exist
     expect(container.querySelector('.logout')).to.not.exist
   })
 })

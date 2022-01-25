3d1928443b9436303b5795bc66fd72b6a0ccdf18
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-01-25 13:35:04 +0200

    #180944819 Remove client-side Google OAuth library and replace logout button with a redirect as placeholder

--- src/common/GoogleAuth.svelte
+++ src/common/GoogleAuth.svelte
@@ -1,69 +1,13 @@
 <script lang="ts">
-  import {onMount} from 'svelte'
   import type {GoogleAuthUser} from './GoogleAuthUser'
-  import jsonLoader from './JsonLoader'
 
   export let user: GoogleAuthUser = undefined
 
-  let GoogleAuth
-  let clientId: string
-
-  onMount(async () => {
-    await loadAuthInfo()
-    if (clientId) await init()
-  })
-
-  interface AuthInfo {
-    google: {client_id: string}
-  }
-
-  async function loadAuthInfo() {
-    const authFile = await tryLoadAuthFile()
-    if (!authFile) return
-    clientId = authFile.google.client_id
-  }
-
-  async function tryLoadAuthFile(): Promise<AuthInfo> {
-    return jsonLoader.loadJson('auth.json').catch(e => console.warn(e))
-  }
-
-  async function handleLogin() {
-    try {
-      await GoogleAuth.signIn()
-    } catch {}
-    checkForLoggedInUser()
-  }
-
-  function checkForLoggedInUser() {
-    if (GoogleAuth.isSignedIn.get()) user = GoogleAuth.currentUser.get().getBasicProfile()
-    else user = undefined
-  }
-
-  async function init() {
-    gapi.load('auth2', () => {
-      GoogleAuth = gapi.auth2.init({
-        client_id: clientId
-      })
-      GoogleAuth.then(checkForLoggedInUser)
-    })
-  }
-
   async function handleLogout() {
-    let auth = gapi.auth2.getAuthInstance()
-    await auth.signOut().then(() => {
-      checkForLoggedInUser()
-    })
+    window.location.replace(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/logout`)
   }
 </script>
 
-{#if clientId}
-  {#if user}
-    <button class="btn btn-outline-secondary logout" on:click={handleLogout}>
-      <i class="fas fa-sign-out-alt"></i> {user.getName()}
-    </button>
-  {:else}
-    <button class="btn btn-outline-secondary login" on:click={handleLogin}>
-      <i class="fab fa-google"></i> Login
-    </button>
-  {/if}
-{/if}
+<button class="btn btn-outline-secondary logout" on:click={handleLogout}>
+  <i class="fas fa-sign-out-alt"></i> Log out
+</button>

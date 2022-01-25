cc15bbf9952d63169a8a6064f708ecff79d60a77
Ian Syalik <ian@codeborne.com>
2022-01-25 15:40:30 +0200

    change padding

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -1,6 +1,6 @@
-<div class="bg-white outline navbar-light navbar-expand-lg ">
+<div class="bg-white outline navbar-light navbar-expand-lg px-3">
   <nav id="top" class="navbar">
-    <div class="container-fluid">
+    <div class="container-fluid p-0">
       <a class="navbar-brand text-dark"><h3>Translation Tool <i class="fas fa-globe"></i></h3></a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,9 +13,3 @@
   </nav>
   <slot name="authenticated"/>
 </div>
-
-<style>
-  nav {
-    padding: 0;
-  }
-</style>

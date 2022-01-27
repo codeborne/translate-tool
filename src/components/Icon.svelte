afa70914e6cf5e06efa629f5fe073e9cac3df236
Ian Syalik <ian@codeborne.com>
2022-01-27 11:21:03 +0200

    add variable for stroke width

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -4,9 +4,10 @@
   export let name: string
   export let size = '1.5rem'
   export let focusable = false
+  export let strokeWidth = '1.5'
 
   const icon = icons[name]
 </script>
 
-<svg class="{$$props.class} icon-{name}" fill="currentColor" {focusable}
+<svg class="{$$props.class} icon-{name}" stroke-width="{strokeWidth}" fill="currentColor" {focusable}
      width={size} height={size} viewBox="0 0 {icon.box} {icon.box}">{@html icon.svg}</svg>
--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,8 +1,9 @@
+export let strokeWidth = '1.5'
 export default {
 
   plus: {
     box: 24,
-    svg: `<g stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    svg: `<g stroke-width="{strokeWidth}" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" /></g>`
   }

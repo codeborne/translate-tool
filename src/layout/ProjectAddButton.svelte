b2764d5515613ab8f976f3bea17075ca310ae198
Ian Syalik <ian@codeborne.com>
2022-01-27 11:21:35 +0200

    change font awesome icon to tabler icon

--- src/layout/ProjectAddButton.svelte
+++ src/layout/ProjectAddButton.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let showAddProject: boolean
+  import Icon from '.././components/Icon.svelte'
 </script>
 
 {#if !showAddProject}
-  <button on:click={() => showAddProject = !showAddProject} class="btn btn-secondary" type="button"
+  <button on:click={() => showAddProject = !showAddProject} class="btn btn-icon btn-secondary" type="button"
           title="Add a new project">
-    <i class="me-2 fas fa-plus-circle"></i>Add a new project
+    <Icon name="plus" size="24"/>
+    <span>Add a new project</span>
   </button>
 {/if}

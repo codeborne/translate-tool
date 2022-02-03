f6591248a140e3f1ee780f73eafc666e96d8ae04
Ian Syalik <ian@codeborne.com>
2022-02-03 14:22:35 +0200

    comment out back button

--- src/config/TogglePagesButton.svelte
+++ src/config/TogglePagesButton.svelte
@@ -12,9 +12,9 @@
       <Icon name="gear"/>Project Settings
     </button>
   {/if}
-  {#if (showConfig || showAddProject) && showBack}
-    <button id="backToEditorBtn" type="button" class="btn btn-icon btn-secondary" on:click={() => showConfig = showAddProject = false}>
-      <Icon name="arrowLeft"/>Back to Editor
-    </button>
-  {/if}
+  <!--{#if (showConfig || showAddProject) && showBack}-->
+  <!--  <button id="backToEditorBtn" type="button" class="btn btn-icon btn-secondary" on:click={() => showConfig = showAddProject = false}>-->
+  <!--    <Icon name="arrowLeft"/>Back to Editor-->
+  <!--  </button>-->
+  <!--{/if}-->
 </div>

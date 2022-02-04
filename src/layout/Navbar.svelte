bad0fd6acfdafe46eb818afd5321103d12f77060
Ian Syalik <ian@codeborne.com>
2022-02-04 11:51:35 +0200

    style toggler button

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -3,9 +3,10 @@
     <div class="container-fluid p-0">
       <a class="navbar-brand text-dark fw-bolder p-0 me-lg-4"><img src="./media/logo_full.svg" class="nav-logo" alt="Translation tool logo"></a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler collapsed border-0" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="animated-icon"><span></span><span></span><span></span><span></span></div>
       </button>
+
       <div class="collapse navbar-collapse gap-2" id="navbarText">
         <slot/>
       </div>

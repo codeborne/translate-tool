d08efde74063311495b6a4afa6faa3823743133c
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-02-14 09:52:26 +0200

    Add additional test for ToggleBackButton.test.ts

--- src/config/ToggleBackButton.test.ts
+++ src/config/ToggleBackButton.test.ts
@@ -5,8 +5,15 @@ import ToggleBackButton from './ToggleBackButton.svelte'
 describe('ToggleBackButton', () => {
   let showConfig = true
   let showAddProject = false
+  let showBack = true
   it('renders', async () => {
-    const {container} = render(ToggleBackButton, {showConfig, showAddProject})
+    const {container} = render(ToggleBackButton, {showConfig, showAddProject, showBack})
     expect(container.querySelector('.backBtn')).to.exist
+  })
+
+  it('does not render', async () => {
+    showBack = false
+    const {container} = render(ToggleBackButton, {showConfig, showAddProject, showBack})
+    expect(container.querySelector('.backBtn')).to.not.exist
   })
 })

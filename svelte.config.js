4b107e229b6e70fa1a885c3bd9a2fefb43b01af9
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-03-14 10:58:50 +0200

    #181027431 Ignore some accessibility warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,17 @@
 const autoPreprocess = require('svelte-preprocess')
 
+const warningsToIgnore = [
+  'a11y-autofocus',
+  'a11y-no-onchange',
+  'a11y-missing-attribute',
+  'a11y-label-has-associated-control'
+]
+
 module.exports = {
   preprocess: autoPreprocess(),
+  onwarn: (warning, handler) => {
+    if (warningsToIgnore.includes(warning.code)) return
+    handler(warning)
+  },
 }
+

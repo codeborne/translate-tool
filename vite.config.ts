3ab01fa3b45defc6fdb2416a86e5ae2395a77cb9
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-03-14 10:38:00 +0200

    #181027431 Move index.html to project root & style imported in source

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+    }
+  },
   plugins: [svelte()]
 })

09fc3c64c030d3cfa99f035d2806853724383527
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-03-21 13:44:00 +0200

    Remove semicolons & use single quotes, for project code style to be consistent

--- src/translator/translate.ts
+++ src/translator/translate.ts
@@ -1,44 +1,42 @@
 import sM from './sM'
 import {getCode, isSupported} from './languages'
 
-// TODO: remove semicolons & use single quotes, for project code style to be consistent
-
 interface TranslateOptions {
-  from: string;
-  to: string;
-  hl: string;
-  raw: boolean;
-  tld: string;
+  from: string
+  to: string
+  hl: string
+  raw: boolean
+  tld: string
 }
 
 interface Token {
-  name: string;
-  value: string;
+  name: string
+  value: string
 }
 
 function token(text: string) {
   return new Promise<Token>(resolve => {
-    resolve({ name: "tk", value: sM(text) });
-  });
+    resolve({ name: 'tk', value: sM(text) })
+  })
 }
 
 // TODO: wrap everything into an object/instance (e.g. translator, same way as Jsonloader)
-let CORSService: string = "";
+let CORSService: string = ''
 
 // setup your own cors-anywhere server
 export const setCORS = (CORSURL: string) => {
-  CORSService = CORSURL;
-  return translate;
-};
+  CORSService = CORSURL
+  return translate
+}
 
 function serialize(obj: object) {
-  let str = [];
+  let str = []
   for (let p in obj)
     if (obj.hasOwnProperty(p)) {
       // @ts-ignore
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
     }
-  return str.join("&");
+  return str.join('&')
 }
 
 export function translate(
@@ -46,50 +44,50 @@ export function translate(
   opts_: { from?: string; to?: string; hl?: string; tld?: string; raw?: boolean } = {}
 ) {
   const opts: TranslateOptions = {
-    from: opts_.from || "auto",
-    to: opts_.to || "en",
-    hl: opts_.hl || "en",
+    from: opts_.from || 'auto',
+    to: opts_.to || 'en',
+    hl: opts_.hl || 'en',
     raw: opts_.raw || false,
-    tld: opts_.tld || "com"
-  };
+    tld: opts_.tld || 'com'
+  }
 
   let e: Error | null = null;
   [opts.from, opts.to].forEach(lang => {
     if (lang && !isSupported(lang)) {
-      e = new Error();
-      e.message = "The language '" + lang + "' is not supported";
+      e = new Error()
+      e.message = `The language '${lang}' is not supported`
     }
-  });
+  })
 
   if (e) {
     return new Promise((resolve, reject) => {
-      reject(e);
-    });
+      reject(e)
+    })
   }
 
   return token(text)
     .then((token: Token) => {
       const url =
-        "https://translate.google." + opts.tld + "/translate_a/single";
+        'https://translate.google.' + opts.tld + '/translate_a/single'
       const data = {
-        client: "gtx",
+        client: 'gtx',
         sl: getCode(opts.from),
         tl: getCode(opts.to),
         hl: getCode(opts.hl),
-        ie: "UTF-8",
-        oe: "UTF-8",
+        ie: 'UTF-8',
+        oe: 'UTF-8',
         otf: 1,
         ssel: 0,
         tsel: 0,
         kc: 7,
         q: text,
         [token.name]: token.value
-      };
+      }
       // @ts-ignore
       let stringifiedData = new URLSearchParams(data).toString()
-      let position: number = stringifiedData.indexOf("hl=") + 6
-      let encoded = [stringifiedData.slice(0, position), 'dt=at&bd=ex&ld=md&qca=rw&rm=ss&t', stringifiedData.slice(position)].join('');
-      return url + "?" + encoded
+      let position: number = stringifiedData.indexOf('hl=') + 6
+      let encoded = [stringifiedData.slice(0, position), 'dt=at&bd=ex&ld=md&qca=rw&rm=ss&t', stringifiedData.slice(position)].join('')
+      return url + '?' + encoded
 
     })
     .then(url => {
@@ -101,15 +99,15 @@ export function translate(
           return result[5][0][2][0][0]
         })
         .catch(err => {
-          const e: Error = new Error();
+          const e: Error = new Error()
           if (err.statusCode !== undefined && err.statusCode !== 200) {
-            e.message = "BAD_REQUEST";
+            e.message = 'BAD_REQUEST'
           } else {
-            e.message = "BAD_NETWORK";
+            e.message = 'BAD_NETWORK'
           }
-          throw e;
-        });
-    });
+          throw e
+        })
+    })
 }
 
-export default translate;
+export default translate

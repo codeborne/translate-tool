bf5c87fcbed7135f9911b0ab059c2f6db4fb8fff
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-03-21 12:11:21 +0200

    Remove removed project prop

--- src/config/ProjectImportList.test.ts
+++ src/config/ProjectImportList.test.ts
@@ -1,16 +1,10 @@
 import {render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import ProjectImportList from './ProjectImportList.svelte'
-import type {Project} from '../common/Project'
 
 describe('ProjectImportList', () => {
-
-  const projects: Project[] = [
-    {title:'project', url:'someurl', indent: 2, token:'123123'}
-  ]
-
   it('renders each individual import option', async () => {
-    const {container} = render(ProjectImportList, {projects})
+    const {container} = render(ProjectImportList)
     expect(container.querySelector('.githubImport')!.textContent).to.contain('Via GitHub')
     expect(container.querySelector('.publicImport')!.textContent).to.contain('Via Public URL')
   })

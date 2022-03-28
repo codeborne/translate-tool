7ec8b5cee821546585fb49bd09917247f8923ccb
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-03-28 15:58:36 +0300

    #181694528 Add in-progress/unfinished rebuild function placeholder

--- src/editor/cleanEmptyKeys.ts
+++ src/editor/cleanEmptyKeys.ts
@@ -1,3 +1,5 @@
+import type {Dict} from '../common/Project'
+
 function isEmpty(value: object) {
   return !Object.keys(value).length
 }
@@ -7,6 +9,18 @@ export function cleanEmptyKeys(dict: Record<string, any>) {
     const isObject = typeof value === 'object'
     if (isObject) value = cleanEmptyKeys(value)
     if (!value || isObject && isEmpty(value)) delete dict[key]
+  }
+  return dict
+}
+
+export function rebuild(dict: Dict, defaultDict: Dict) {
+  for (let [key] of Object.entries(defaultDict)) {
+    const isObject = typeof dict[key] === 'object'
+    if (isObject) dict[key] = rebuild(dict[key], defaultDict[key])
+    if (!dict[key] || isObject && isEmpty(dict[key])) {
+      delete dict[key]
+    }
+    dict[key] = defaultDict[key]
   }
   return dict
 }

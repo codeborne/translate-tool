02bf2cf6b8bf7ae3af56466aac2fba24409b1798
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-04 11:41:05 +0300

    #181694855 Add initial excluded keys class

---
+++ src/common/ExcludeKeys.ts
@@ -0,0 +1,31 @@
+import jsonLoader from './JsonLoader'
+import type {Project} from './Project'
+import {BitBucketClient} from '../bitbucket/BitBucketClient'
+import {GitHubClient} from '../github/GitHubClient'
+
+class ExcludeKeys {
+  fileName = 'dont-translate-keys.json'
+
+  async fetch(project: Project) {
+    if (project.url.includes(BitBucketClient.host)) return await this.fetchBitBucket(project)
+    else if (project.url.includes(GitHubClient.host)) return await this.fetchGithub(project)
+    else return await this.fetchPublic(project.url)
+  }
+
+  async fetchPublic(url: string) {
+    return await jsonLoader.loadJson(url + this.fileName)
+  }
+
+  fetchGithub(project: Project) {
+    const client = new GitHubClient(project)
+    return client.getFileContent(this.fileName)
+  }
+
+  fetchBitBucket(project: Project) {
+    const client = new BitBucketClient(project)
+    return client.getFileContent(this.fileName)
+  }
+
+}
+
+export const excluded = new ExcludeKeys()

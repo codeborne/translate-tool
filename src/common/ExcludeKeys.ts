8571d5d0c171821189529c575d3890152ef3f22c
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-04 13:45:32 +0300

    #181694855 Simplify excludekeys, as similar logic is already present in jsonloader

--- src/common/ExcludeKeys.ts
+++ src/common/ExcludeKeys.ts
@@ -1,31 +1,18 @@
 import jsonLoader from './JsonLoader'
 import type {Project} from './Project'
-import {BitBucketClient} from '../bitbucket/BitBucketClient'
-import {GitHubClient} from '../github/GitHubClient'
 
-class ExcludeKeys {
-  fileName = 'dont-translate-keys.json'
+class ExcludedKeys {
+  private fileName = 'dont-translate-keys'
 
-  async fetch(project: Project) {
-    if (project.url.includes(BitBucketClient.host)) return await this.fetchBitBucket(project)
-    else if (project.url.includes(GitHubClient.host)) return await this.fetchGithub(project)
-    else return await this.fetchPublic(project.url)
+  public async fetch(project: Project) {
+    let excludedKeys: string[]
+    try {
+      excludedKeys = await jsonLoader.loadFor(project, this.fileName) as string[]
+    } catch (e) {
+      excludedKeys = []
+    }
+    return excludedKeys ?? []
   }
-
-  async fetchPublic(url: string) {
-    return await jsonLoader.loadJson(url + this.fileName) ?? []
-  }
-
-  fetchGithub(project: Project) {
-    const client = new GitHubClient(project)
-    return client.getFileContent(this.fileName) ?? []
-  }
-
-  fetchBitBucket(project: Project) {
-    const client = new BitBucketClient(project)
-    return client.getFileContent(this.fileName) ?? []
-  }
-
 }
 
-export const excluded = new ExcludeKeys()
+export const excluded = new ExcludedKeys()

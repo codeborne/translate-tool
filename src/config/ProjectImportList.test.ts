0d7de40ab443d5b2c232cb75afe3705852fe8816
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-08 14:41:21 +0300

    #181622491 Test ProjectImportList accordion behaviour when toggled by clicking

--- src/config/ProjectImportList.test.ts
+++ src/config/ProjectImportList.test.ts
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/svelte'
+import {fireEvent, render} from '@testing-library/svelte'
 import {expect} from 'chai'
 import ProjectImportList from './ProjectImportList.svelte'
 
@@ -8,5 +8,19 @@ describe('ProjectImportList', () => {
     expect(container.querySelector('.githubImport')!.textContent).to.contain('Via GitHub')
     expect(container.querySelector('.publicImport')!.textContent).to.contain('Via Public URL')
     expect(container.querySelector('.bitbucketImport')!.textContent).to.contain('Via BitBucket')
+  })
+
+  it('opens content on toggle event', async () => {
+    const {container} = render(ProjectImportList)
+    expect(container.querySelector('.githubImport .accordion-content')).to.not.exist
+    expect(container.querySelector('.publicImport .accordion-content')).to.exist
+
+    let accordion = container.querySelector('.githubImport .accordion-button')!
+    await fireEvent.click(accordion)
+    expect(container.querySelector('.githubImport .accordion-content')).to.exist
+
+    accordion = container.querySelector('.bitbucketImport .accordion-button')!
+    await fireEvent.click(accordion)
+    expect(container.querySelector('.bitbucketImport .accordion-content')).to.exist
   })
 })

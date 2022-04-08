2d37d0da3a755e69a6ed9a478436cb38670702c2
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-08 14:35:16 +0300

    #181622491 Use a list to render accordions for the ability to only have one accordion opened at a time

--- src/config/ProjectImportList.svelte
+++ src/config/ProjectImportList.svelte
@@ -3,21 +3,36 @@
   import GitHubProjectImporter from './GitHubProjectImporter.svelte'
   import BitBucketProjectImporter from './BitBucketProjectImporter.svelte'
   import Accordion from './Accordion.svelte'
+  import {SvelteComponent} from 'svelte'
+
+  interface Accordion {
+    message: string,
+    className: string,
+    slot: SvelteComponent,
+    isOpen?: boolean
+  }
+
+  let importers: Accordion[] = [
+    { message: 'Via GitHub', className: 'githubImport', slot: GitHubProjectImporter },
+    { message: 'Via BitBucket', className: 'bitbucketImport', slot: BitBucketProjectImporter },
+    { message: 'Via Public URL', className: 'publicImport', isOpen: true, slot: SimpleProjectImporter },
+  ] as Accordion[]
+
+  function toggle(e) {
+    importers.forEach(i => i.isOpen = i.className === e.detail && i.isOpen !== true)
+    importers = importers
+  }
+
 </script>
 
 <div class="accordion addNew fix-width mx-auto">
   <h3 class="mb-3">New project</h3>
 
-  <Accordion message="Via GitHub" className="githubImport">
-    <GitHubProjectImporter on:imported />
-  </Accordion>
-
-  <Accordion message="Via BitBucket" className="bitbucketImport">
-    <BitBucketProjectImporter on:imported />
-  </Accordion>
-
-  <Accordion message="Via Public URL" className="publicImport" isOpen={true} >
-    <SimpleProjectImporter on:imported />
-  </Accordion>
+  {#each importers as importer}
+    <Accordion on:toggle={toggle}
+      message={importer.message} className={importer.className} isOpen={importer.isOpen}>
+      <svelte:component this={importer.slot} />
+    </Accordion>
+  {/each}
 
 </div>

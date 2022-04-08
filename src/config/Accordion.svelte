c5641b0fabf9c052c2f2e72b6dec5e001cc58d0b
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-08 11:34:28 +0300

    #181622491 Add accordion toggling and use svelte's transitions

--- src/config/Accordion.svelte
+++ src/config/Accordion.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
+  import {createEventDispatcher} from 'svelte'
+  import {slide} from 'svelte/transition'
+
   export let message
+  export let className
+  export let isOpen = false
+
+  const dispatch = createEventDispatcher()
+
+  const toggle = () => dispatch('toggle')
+  const open = () => isOpen = !isOpen
+
 </script>
 
-<div class="accordion-item shadow mb-3 rounded-3 overflow-hidden">
+<div class="shadow mb-3 rounded-3 overflow-hidden">
+
   <h2 class="accordion-header">
-    <button class="accordion-button p-lg-4 collapsed githubImport" type="button" data-bs-toggle="collapse"
-            data-bs-target=".collapseGithub" aria-expanded="false" aria-controls="collapseThree">
+    <button on:click={open} class:collapsed={!isOpen} class="accordion-button p-lg-4 collapsed {className}" type="button">
       {message}
     </button>
   </h2>
-  <div class="accordion-collapse collapse m-3 m-lg-4 collapseGithub" aria-labelledby="headingThree"
-       data-bs-parent=".addNew">
-    <slot/>
-  </div>
+
+  {#if isOpen}
+    <div class:collapse={!isOpen} transition:slide class="collapsed accordion-content m-3 m-lg-4">
+      <slot />
+    </div>
+  {/if}
+
 </div>

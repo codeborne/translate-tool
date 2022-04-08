5c4bf56b838bd828e9d71c30ec376e5a3caaa1f6
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-08 12:20:19 +0300

    #181622491 Add Accordion component tests

---
+++ src/config/Accordion.test.ts
@@ -0,0 +1,41 @@
+import {fireEvent, render} from '@testing-library/svelte'
+import {expect} from 'chai'
+import Accordion from './Accordion.svelte'
+
+describe('Accordion', () => {
+
+  const message = 'Should display'
+  const className = 'accordionClass'
+
+  it('contains the right class name within class', async () => {
+    const {container} = render(Accordion, {message, className})
+    expect(container.querySelector('.accordionClass')).to.exist
+  })
+
+  it('displays the accordion header', async () => {
+    const {container} = render(Accordion, {message, className})
+    expect(container.querySelector('.accordion-button')).to.exist
+    expect(container.querySelector('.accordion-button')!.textContent).to.contain('Should display')
+  })
+
+  it('is closed by default', async () => {
+    const {container} = render(Accordion, {message, className})
+    expect(container.querySelector('.accordion-content')).to.not.exist
+    expect(container.querySelector('button.collapsed')).to.exist
+  })
+
+  it('shows content when open', async () => {
+    const {container} = render(Accordion, {message, className, isOpen: true})
+    expect(container.querySelector('.accordion-content')).to.exist
+    expect(container.querySelector('button.collapsed')).to.not.exist
+
+  })
+
+  it('opens content on click', async () => {
+    const {container} = render(Accordion, {message, className})
+    expect(container.querySelector('.accordion-content')).to.not.exist
+    const accordion = container.querySelector('.accordion-button')!
+    await fireEvent.click(accordion)
+    expect(container.querySelector('.accordion-content')).to.exist
+  })
+})

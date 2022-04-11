235176f4e97d95090899eb39dbe846e206f8b0c3
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-11 12:19:11 +0300

    Fix ExcludedKeysLoader test as it now uses a set

--- src/common/ExcludedKeysLoader.test.ts
+++ src/common/ExcludedKeysLoader.test.ts
@@ -6,14 +6,14 @@ import jsonLoader from './JsonLoader'
 
 describe('ExcludedKeys', () => {
 
-  const response = ['test1', 'test2']
+  const response: Set<string> = new Set(['test1', 'test2'])
   const project: Project = {
     title: 'Title', url: 'url', indent: 2
   }
 
   it('calls jsonLoader and returns array of strings', async () => {
     stub(jsonLoader, 'loadFor').resolves(response)
-    expect(await excludedKeysLoader.fetch(project)).to.equal(response)
+    expect(await excludedKeysLoader.fetch(project)).to.deep.equal(response)
     expect(jsonLoader.loadFor).calledWith(project, 'dont-translate-keys')
   })
 })

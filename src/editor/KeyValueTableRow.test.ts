359644667652e9db0d71dc1b9a39406f698380a0
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-11 12:34:55 +0300

    #181835125 improve KeyValueTableRow with further detailed excluded keys tests

--- src/editor/KeyValueTableRow.test.ts
+++ src/editor/KeyValueTableRow.test.ts
@@ -12,12 +12,13 @@ describe('<KeyValueTableRow>', () => {
       another: 'worlddd',
       ping: 'pong',
       more_nested: {
-        foo: 'bar'
+        foo: 'bar',
+        bar: 'foo'
       }
     }
   }
 
-  let excluded: string[] = []
+  let excludedKeys: Set<string> = new Set([])
   let lang = 'en'
   const defaultLang: string = 'en'
   const filter = new Filter()
@@ -25,44 +26,65 @@ describe('<KeyValueTableRow>', () => {
   const uneditedDict: Dict = dict
 
   it('renders all inputs', () => {
-    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excluded})
-    // expect(container.querySelectorAll('textarea')).to.have.length(5)
+    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excludedKeys})
     const rows = container.querySelectorAll('tr')
     const firstRow = rows[0].querySelectorAll('td')
     const lastRow = rows[rows.length - 1].querySelectorAll('td')
-
-    // first row
     expect(firstRow).to.have.length(3)
-    // expect(firstRow[0].textContent).to.contain('world')
-    // expect(firstRow[1].querySelector('textarea')!.value).to.contain('hello')
-    // expect(firstRow[2].textContent).to.contain('hello')
-
-    // last row
+    expect(firstRow[0].textContent).to.contain('world')
+    expect(firstRow[2].textContent).to.contain('hello')
     expect(lastRow).to.have.length(3)
-    expect(lastRow[0].textContent).to.contain('nested.more_nested.foo')
-    // expect(lastRow[1].querySelector('textarea')!.value).to.contain('bar')
-    // expect(lastRow[2].textContent).to.contain('bar')
+    expect(lastRow[0].textContent).to.contain('nested.more_nested.bar')
+    expect(lastRow[2].textContent).to.contain('foo')
   })
 
   it('shows only filtered inputs', () => {
     filter.search = 'nested'
-    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excluded})
+    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excludedKeys})
     const inputs = container.querySelectorAll('.not-html')
-    expect(inputs).to.have.length(3)
+    expect(inputs).to.have.length(4)
     expect(inputs[0].textContent).to.contain('worlddd')
     expect(container.textContent).to.not.contain('hello')
   })
 
-  it('does not contain keys that are not supposed to be translated (excluded)', () => {
+  it('does not contain individual keys that are not supposed to be translated', () => {
     filter.search = ''
     lang = 'de'
-    excluded = ['nested.ping']
-    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excluded})
+    excludedKeys = new Set(['nested.ping'])
+    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excludedKeys})
     const inputs = container.querySelectorAll('.not-html')
-    expect(inputs).to.have.length(4)
+    expect(inputs).to.have.length(5)
     expect(container.textContent).to.not.contain('nested.ping')
     expect(container.textContent).to.not.contain('pong')
     expect(container.textContent).to.contain('nested.another')
+  })
 
+  it('does not contain objects that are not supposed to be translated', () => {
+    filter.search = ''
+    lang = 'de'
+    excludedKeys = new Set(['nested.more_nested'])
+    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excludedKeys})
+    const inputs = container.querySelectorAll('.not-html')
+    expect(inputs).to.have.length(4)
+    expect(container.textContent).to.not.contain('nested.more_nested.foo')
+    expect(container.textContent).to.not.contain('nested.more_nested.bar')
+    expect(container.textContent).to.contain('nested.another')
+  })
+
+  it('shows label if cannot be translated but default lang is selected', () => {
+
+    const untranslatableLabel = 'Marked as untranslatable'
+    filter.search = ''
+    lang = 'en'
+    excludedKeys = new Set(['nested.more_nested', 'nested.another'])
+    const {container} = render(KeyValueTableRow, {defaultLang, dict, defaultDict, uneditedDict, filter, lang, excludedKeys})
+    const rows = container.querySelectorAll('tr')
+    expect(rows).to.have.length(6)
+    expect(rows[0].querySelectorAll('td')[0].textContent).to.not.contain(untranslatableLabel)
+    expect(rows[1].querySelectorAll('td')[0].textContent).to.not.contain(untranslatableLabel)
+    expect(rows[2].querySelectorAll('td')[0].textContent).to.contain(untranslatableLabel)
+    expect(rows[3].querySelectorAll('td')[0].textContent).to.not.contain(untranslatableLabel)
+    expect(rows[4].querySelectorAll('td')[0].textContent).to.contain(untranslatableLabel)
+    expect(rows[5].querySelectorAll('td')[0].textContent).to.contain(untranslatableLabel)
   })
 })

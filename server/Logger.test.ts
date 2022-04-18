ef23f2a99a36bf554845c5a8857066cd922d08cc
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-18 13:12:40 +0300

    #181834892 Use a predefined date in test for the timestamp to be known ahead of time

--- server/Logger.test.ts
+++ server/Logger.test.ts
@@ -1,11 +1,29 @@
 import logger from './Logger'
-import sinon from 'sinon'
+import Logger from './Logger'
+import sinon, {stub} from 'sinon'
 import {expect} from 'chai'
+
+const date = Date.parse('9 April 2020 16:30:00 GMT')
+  beforeEach(() => {
+    stub(Date, 'now').returns(date)
+  })
 
 describe('Logger', () => {
   it('outputs error', async () => {
     const log = sinon.spy(console, 'error')
     logger.error('an error')
-    expect(log.calledWithMatch('[ERROR] an error')).to.be.true
+    expect(log.calledWith('[2020-3-1 16:30] [ERROR] an error')).to.be.true
+  })
+
+  it('outputs info log', async () => {
+    const log = sinon.spy(console, 'info')
+    logger.info('an info log')
+    expect(log.calledWith('[2020-3-1 16:30] [INFO] an info log')).to.be.true
+  })
+
+  it('outputs regular log', async () => {
+    const log = sinon.spy(console, 'log')
+    logger.log('some kind of log')
+    expect(log.calledWith('[2020-3-1 16:30] some kind of log')).to.be.true
   })
 })

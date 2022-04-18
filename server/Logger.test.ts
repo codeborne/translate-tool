931ed7398169947088f7fe8d381ca759f6509846
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-04-18 13:25:35 +0300

    #181834892 use Logger instead of logger as variable name to reduce duplicate import statements

--- server/Logger.test.ts
+++ server/Logger.test.ts
@@ -1,4 +1,3 @@
-import logger from './Logger'
 import Logger from './Logger'
 import sinon, {stub} from 'sinon'
 import {expect} from 'chai'
@@ -11,19 +10,19 @@ const date = Date.parse('9 April 2020 16:30:00 GMT')
 describe('Logger', () => {
   it('outputs error', async () => {
     const log = sinon.spy(console, 'error')
-    logger.error('an error')
+    Logger.error('an error')
     expect(log.calledWith('[2020-3-1 16:30] [ERROR] an error')).to.be.true
   })
 
   it('outputs info log', async () => {
     const log = sinon.spy(console, 'info')
-    logger.info('an info log')
+    Logger.info('an info log')
     expect(log.calledWith('[2020-3-1 16:30] [INFO] an info log')).to.be.true
   })
 
   it('outputs regular log', async () => {
     const log = sinon.spy(console, 'log')
-    logger.log('some kind of log')
+    Logger.log('some kind of log')
     expect(log.calledWith('[2020-3-1 16:30] some kind of log')).to.be.true
   })
 })

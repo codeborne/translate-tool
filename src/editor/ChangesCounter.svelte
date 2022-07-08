300380074e6455c02cc7b32ec3e0aa1be1c5a1dd
Markus Tammeoja <markus@codeborne.com>
2022-07-08 15:34:41 +0300

    simplify changesCounter & unify look

--- src/editor/ChangesCounter.svelte
+++ src/editor/ChangesCounter.svelte
@@ -2,6 +2,7 @@
   import {totalDifferentValues} from './languageStats'
   import type {Dict} from '../common/Project'
   import Icon from '../components/Icon.svelte'
+  import {slide} from 'svelte/transition'
 
   export let dict: Dict
   export let uneditedDict: Dict
@@ -11,16 +12,16 @@
   $: if (dict) changes = totalDifferentValues(dict, uneditedDict)
 </script>
 
-<div class="num-changes card h-100 flex-row justify-content-center bg-light align-items-center p-1 px-2 text-secondary shadow-sm rounded-pill order-md-3 me-lg-4
-{!changes ? `d-md-none` : ``}">
-
-  <Icon name="info" class="me-1 text-warning"/>
-  <span>{changes} {changes === 1 ? `change` : `changes`}</span>
-</div>
+{#if changes}
+  <div class="num-changes btn shadow" transition:slide|local>
+    <Icon name="info" class="me-1 text-warning"/>
+    <span>{changes} {changes === 1 ? `change` : `changes`}</span>
+  </div>
+{/if}
 
 <style>
-  .text-secondary {
-    font-size: 0.95em;
-    transition: opacity 0.6s;
+  .num-changes:hover {
+    cursor: default;
+    background-color: white;
   }
 </style>

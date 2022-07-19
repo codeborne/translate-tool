1719cee13ba197e2835ede3fa1d009ff80f44913
Markus Tammeoja <markus.tammeoja@gmail.com>
2022-07-19 16:07:18 +0300

    add quickstart note to those not familiar with the file structure, which includes link to the readme and an example project

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -4,9 +4,10 @@
   export let fullWidth = false
   export let padding = 'p-3 pb-4 pt-lg-4 pb-lg-5 px-lg-5'
   export let flex = 'd-flex flex-column justify-content-center align-items-center'
+  export let transparent = false
 </script>
 
-<div class="{flex} {fullWidth ? '' : 'fix-width'} {padding} card shadow rounded-3 mx-auto {$$props.class ?? ''}"
+<div class="{flex} {fullWidth ? '' : 'fix-width'} {padding} {transparent ? '' : 'card shadow'} rounded-3 mx-auto {$$props.class ?? ''}"
      in:fly={{x: -200, duration: 200, local: true}}>
   <slot/>
 </div>

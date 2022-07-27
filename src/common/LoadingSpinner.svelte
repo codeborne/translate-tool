dd2af33598495548becd0b795ecede15e8e8adc9
Markus Tammeoja <markus@codeborne.com>
2022-07-28 01:52:06 +0300

    use transition delay instead in spinner to keep it simpler

--- src/common/LoadingSpinner.svelte
+++ src/common/LoadingSpinner.svelte
@@ -1,3 +1,7 @@
-<div class="d-flex justify-content-center align-center {$$props.class ?? ''}">
+<script lang="ts">
+  import {fade} from 'svelte/transition'
+</script>
+
+<div class="d-flex justify-content-center align-center {$$props.class ?? ''}" in:fade={{delay: 50}}>
   <div class="spinner-border"></div>
 </div>

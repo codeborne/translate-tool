910cd8c3232cc38b0dcd827193bd3e19e3ece291
Markus Tammeoja <markus@codeborne.com>
2022-07-29 00:04:35 +0300

    specify search that it's filtering by key and set min width & height

--- src/editor/FilterControls.svelte
+++ src/editor/FilterControls.svelte
@@ -5,14 +5,19 @@
   export let filter: Filter
 </script>
 
-<div class="d-flex align-items-center justify-content-start w-50">
-  <div class="w-50 pe-3">
-    <input class="form-control" bind:value={filter.search} placeholder="Filter...">
-  </div>
+<div class="d-flex gap-3 align-items-center justify-content-start w-50">
+  <input class="form-control filter" bind:value={filter.search} placeholder="Filter by key...">
   <div class="form-check form-switch ms-2">
     <input class="form-check-input" title="Show only empty" type="checkbox" bind:checked={filter.showEmptyValuesOnly} id="show-empty-only">
     <label class="form-check-label text-nowrap" for="show-empty-only">Show only empty</label>
   </div>
   <slot/>
 </div>
+
+<style>
+  .filter {
+    max-width: 300px !important;
+    min-width: 150px !important;
+  }
+</style>
 

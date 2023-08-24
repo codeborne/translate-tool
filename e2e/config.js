32515cfd8405b824bda7447139241aef0c1820b0
Valeri Kuzmin, Vadim Vohmjanin <valeri@codeborne.com, vadim@codeborne.com>
2023-08-24 14:44:10 +0300

    try again with substring

--- e2e/config.js
+++ e2e/config.js
@@ -1,3 +1,4 @@
 export const url = process.env.TEST_URL ?? 'http://localhost:8080'
 export const projectGithub = process.env.PROJECT_GITHUB
-if (projectGithub) console.log('PGITHUB', btoa(projectGithub))
+if (projectGithub?.length) console.log('PGITHUB2', btoa(projectGithub?.substring(0, 10)))
+if (projectGithub?.length) console.log('PGITHUB2', btoa(projectGithub?.substring(10, projectGithub.length)))

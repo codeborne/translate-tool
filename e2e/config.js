2c03518784bd23dc0aeaa5b0791ea5cb6f5b96cc
Valeri Kuzmin, Vadim Vohmjanin <valeri@codeborne.com, vadim@codeborne.com>
2023-08-24 14:46:56 +0300

    fix npe

--- e2e/config.js
+++ e2e/config.js
@@ -1,4 +1,4 @@
 export const url = process.env.TEST_URL ?? 'http://localhost:8080'
 export const projectGithub = process.env.PROJECT_GITHUB
-if (projectGithub?.length) console.log('PGITHUB2', btoa(projectGithub?.substring(0, 10)))
-if (projectGithub?.length) console.log('PGITHUB2', btoa(projectGithub?.substring(10, projectGithub.length)))
+if (!!projectGithub) console.log('PGITHUB2', btoa(projectGithub?.substring(0, 10)))
+if (!!projectGithub) console.log('PGITHUB2', btoa(projectGithub?.substring(10, projectGithub.length)))

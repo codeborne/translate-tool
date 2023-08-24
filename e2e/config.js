160712bb062221bed2167f5e25f66072bacf818f
Valeri Kuzmin, Vadim Vohmjanin <valeri@codeborne.com, vadim@codeborne.com>
2023-08-24 14:55:43 +0300

    remove debugging code

--- e2e/config.js
+++ e2e/config.js
@@ -1,4 +1,2 @@
 export const url = process.env.TEST_URL ?? 'http://localhost:8080'
 export const projectGithub = process.env.PROJECT_GITHUB
-if (!!projectGithub) console.log('PGITHUB2', projectGithub?.substring(0, 10))
-if (!!projectGithub) console.log('PGITHUB2', projectGithub?.substring(10, projectGithub.length))
